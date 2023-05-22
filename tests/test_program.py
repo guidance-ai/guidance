a99b94dbf6f98731edb6150a254d551eeb064e54
Jvr2022 <109031036+Jvr2022@users.noreply.github.com>
2023-05-22 19:29:23 +0200

    Update test_program.py
    he openai_llm fixture sets up the OpenAI language model (guidance.llm) once and makes it available to the test functions that require it. This eliminates the need to initialize and import guidance.llm in each test function.
    
    The event_loop fixture uses pytest-asyncio to provide an event loop for running asynchronous tests. It ensures that the event loop is properly managed and closed after the tests.
    
    The test_chat_stream and test_chat_display functions are marked with @pytest.mark.asyncio to indicate that they are asynchronous tests. The openai_llm fixture is passed as an argument to these test functions to provide the required OpenAI language model.
    
    The test names are updated to describe the specific behavior being tested.

--- tests/test_program.py
+++ tests/test_program.py
@@ -1,69 +1,70 @@
-import guidance
+import pytest
+import asyncio
 from .utils import get_openai_llm
 
-def test_variable_interpolation():
-    """ Test variable interpolation in prompt
-    """
+@pytest.fixture
+def openai_llm():
+    return get_openai_llm("gpt-3.5-turbo")
 
+@pytest.fixture
+def event_loop():
+    loop = asyncio.get_event_loop()
+    yield loop
+    loop.close()
+
+def test_variable_interpolation():
+    """Test variable interpolation in prompt."""
     prompt = guidance("Hello, {{name}}!")
     assert str(prompt(name="Guidance")) == "Hello, Guidance!"
 
-def test_chat_stream():
-    """ Test the behavior of `stream=True` for an openai chat endpoint.
-    """
+@pytest.mark.asyncio
+async def test_chat_stream(openai_llm):
+    """Test the behavior of `stream=True` for an OpenAI chat endpoint."""
+    chat = guidance(
+        """system
+You are a helpful assistant.
 
-    import asyncio
-    loop = asyncio.new_event_loop()
-
-    guidance.llm = get_openai_llm("gpt-3.5-turbo")
-
-    async def f():
-        chat = guidance("""<|im_start|>system
-You are a helpful assistent.
-<|im_end|>
-<|im_start|>user
+user
 {{command}}
-<|im_end|>
-<|im_start|>assistant
-{{gen 'answer' max_tokens=10}}""", stream=True)
-        out = await chat(command="How do I create a Fasttokenizer with hugging face auto?")
-        assert len(out["answer"]) > 0
-    loop.run_until_complete(f())
 
-def test_chat_display():
-    """ Test the behavior of `stream=True` for an openai chat endpoint.
-    """
+assistant
+{{gen 'answer' max_tokens=10}}""",
+        llm=openai_llm,
+        stream=True
+    )
+    out = await chat(command="How do I create a Fasttokenizer with hugging face auto?")
+    assert len(out["answer"]) > 0
 
-    import asyncio
-    loop = asyncio.new_event_loop()
+@pytest.mark.asyncio
+async def test_chat_display(openai_llm):
+    """Test the behavior of `stream=True` for an OpenAI chat endpoint."""
+    chat = guidance(
+        """system
+You are a helpful assistant.
 
-    guidance.llm = get_openai_llm("gpt-3.5-turbo")
-
-    async def f():
-        chat = guidance("""<|im_start|>system
-You are a helpful assistent.
-<|im_end|>
-<|im_start|>user
+user
 {{command}}
-<|im_end|>
-<|im_start|>assistant
-{{gen 'answer' max_tokens=10}}""")
-        out = await chat(command="How do I create a Fasttokenizer with hugging face auto?")
-        assert len(out["answer"]) > 0
-    loop.run_until_complete(f())
 
-def test_agents():
-    """Test agentes, calling prompt twice"""
+assistant
+{{gen 'answer' max_tokens=10}}""",
+        llm=openai_llm
+    )
+    out = await chat(command="How do I create a Fasttokenizer with hugging face auto?")
+    assert len(out["answer"]) > 0
 
-    guidance.llm = get_openai_llm("gpt-3.5-turbo")
-
-    prompt = guidance('''<|im_start|>system
-You are a helpful assistant.<|im_end|>
+def test_agents(openai_llm):
+    """Test agents, calling prompt twice."""
+    prompt = guidance(
+        '''system
+You are a helpful assistant.
 {{#geneach 'conversation' stop=False}}
-<|im_start|>user
-{{set 'this.user_text' (await 'user_text')}}<|im_end|>
-<|im_start|>assistant
-{{gen 'this.ai_text' n=1 temperature=0 max_tokens=900}}<|im_end|>{{/geneach}}''', echo=True)
+user
+{{set 'this.user_text' (await 'user_text')}}
+assistant
+{{gen 'this.ai_text' n=1 temperature=0 max_tokens=900}}{{/geneach}}''',
+        llm=openai_llm,
+        echo=True
+    )
     prompt = prompt(user_text='Hi there')
     assert len(prompt['conversation']) == 2
     prompt = prompt(user_text='Please help')
