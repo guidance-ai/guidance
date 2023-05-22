38a4c83716dd393f70df3e15dbc21fd7a200fa5b
Jvr2022 <109031036+Jvr2022@users.noreply.github.com>
2023-05-22 19:33:40 +0200

    Update test_transformers.py
    The transformers_llm fixture sets up the Transformers language model (llm) once and makes it available to the test functions that require it. This eliminates the need to initialize and import get_transformers_llm in each test function.
    
    The test names are updated to describe the specific behavior being tested.

--- tests/llms/test_transformers.py
+++ tests/llms/test_transformers.py
@@ -1,23 +1,25 @@
-import guidance
+import pytest
 from ..utils import get_transformers_llm
 
-def test_basic():
-    llm = get_transformers_llm('gpt2')
+@pytest.fixture
+def transformers_llm():
+    return get_transformers_llm('gpt2')
+
+def test_basic(transformers_llm):
+    llm = transformers_llm
     with llm.session() as s:
         out = s("this is a test", max_tokens=5)
         print(out)
 
-def test_repeat():
-    llm = get_transformers_llm('gpt2')
+def test_repeat(transformers_llm):
+    llm = transformers_llm
     with llm.session() as s:
         out1 = s("this is a test", max_tokens=5)
         out2 = s("this is a test like another", max_tokens=5)
         print(out2)
 
-
-
-def test_select():
-    llm = get_transformers_llm('gpt2')
+def test_select(transformers_llm):
+    llm = transformers_llm
     program = guidance('''Answer "yes" or "no": "{{#select 'answer'}}yes{{or}}no{{/select}}"''', llm=llm)
     out = program()
     assert out["answer"] in ["yes", "no"]
