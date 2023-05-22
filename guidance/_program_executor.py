03c3defe1b65e6232078e3e7add717ccf32da1d0
Scott Lundberg <scott.lundberg@microsoft.com>
2023-05-22 09:56:45 -0700

    Fix next_node for block commands

--- guidance/_program_executor.py
+++ guidance/_program_executor.py
@@ -385,8 +385,8 @@ class ProgramExecutor():
                         "block_content": self.block_content[-1],
                         "partial_output": self.extend_prefix,
                         "parser_node": node,
-                        "next_node": node.children[-1],
-                        "next_next_node": next_node,
+                        "next_node": next_node,
+                        "next_next_node": next_next_node,
                         "prev_node": node.children[0]
                     }
                 
