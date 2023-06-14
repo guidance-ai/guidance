c84e693bcb1dc4003d5bdf445d7558c8006a4f8b
Yuta Nishi <riyaamemiya@gmail.com>
2023-06-14 10:35:58 +0900

    add new models

--- guidance/llms/_openai.py
+++ guidance/llms/_openai.py
@@ -64,8 +64,12 @@ chat_models = [
     "gpt-4-32k",
     "gpt-4-0314",
     "gpt-4-32k-0314",
+    "gpt-4-0613",
+    "gpt-4-32k-0613"
     "gpt-3.5-turbo",
-    "gpt-3.5-turbo-0301"
+    "gpt-3.5-turbo-16k",
+    "gpt-3.5-turbo-0301",
+    "gpt-3.5-turbo-0613"
 ]
 
 class OpenAI(LLM):
