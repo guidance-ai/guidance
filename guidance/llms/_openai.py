cad7dd058cf163c3d12e511001c11c22bd8c0526
Yuta Nishi <riyaamemiya@gmail.com>
2023-06-14 19:03:54 +0900

    fix bugs

--- guidance/llms/_openai.py
+++ guidance/llms/_openai.py
@@ -65,7 +65,7 @@ chat_models = [
     "gpt-4-0314",
     "gpt-4-32k-0314",
     "gpt-4-0613",
-    "gpt-4-32k-0613"
+    "gpt-4-32k-0613",
     "gpt-3.5-turbo",
     "gpt-3.5-turbo-16k",
     "gpt-3.5-turbo-0301",
