4fd5435b3ba86ec3d26ec73c94ca06c0978d0621
Dani Vilanova <thedavigo95@gmail.com>
2023-08-09 09:51:43 +0000

    Update llama model in tests

--- tests/llms/transformers/test_llama.py
+++ tests/llms/transformers/test_llama.py
@@ -11,6 +11,6 @@ def test_basic():
         pytest.skip("No GPU, so skipping large model test.")
 
     # just make sure it runs
-    llm = guidance.llms.transformers.LLaMA('../../models/llama/7B', device=1)
+    llm = guidance.llms.transformers.LLaMA('huggyllama/llama-7b', device=0)
     out = guidance("""The height of the Sears tower is {{gen 'answer' max_tokens=10}}""", llm=llm)()
     assert len(out["answer"]) > 0
