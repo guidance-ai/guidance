af413279c75534fa51ebebba8f8e4b698b788c45
Harsha Nori <harsha.nori@live.com>
2023-09-08 17:17:16 -0700

    basic WIP tests for pythonic branch

---
+++ tests/utils_pythonic.py
@@ -0,0 +1,41 @@
+import guidance
+import pytest
+
+opanai_model_cache = {}
+transformers_model_cache = {}
+
+def get_llm(model_name, caching=False, **kwargs):
+    """ Get an LLM by name.
+    """
+    if model_name.startswith("openai:"):
+        return get_openai_lm(model_name[7:], caching, **kwargs)
+    elif model_name.startswith("transformers:"):
+        return get_transformers_lm(model_name[13:], caching, **kwargs)
+
+def get_openai_lm(model_name, caching=False, **kwargs):
+    """ Get an OpenAI LLM with model reuse and smart test skipping.
+    """
+
+    # we cache the models so lots of tests using the same model don't have to
+    # load it over and over again
+    key = model_name+"_"+str(caching)
+    if key not in opanai_model_cache:
+        opanai_model_cache[key] = guidance.models.OpenAI(model_name, caching=caching, **kwargs)
+    llm = opanai_model_cache[key]
+
+    if llm.endpoint.api_key is None:
+        pytest.skip("OpenAI token not found")
+
+    return llm
+
+def get_transformers_lm(model_name, caching=False, **kwargs):
+    """ Get a Transformers LLM with model reuse and smart test skipping.
+    """
+
+    # we cache the models so lots of tests using the same model don't have to
+    # load it over and over again
+    key = model_name+"_"+str(caching)
+    if key not in transformers_model_cache:
+        transformers_model_cache[key] = guidance.models.Transformers(model_name, caching=caching, **kwargs)
+
+    return transformers_model_cache[key]
