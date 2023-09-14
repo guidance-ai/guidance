99ffd0a255f1c30287320ecae6027e069e294bba
Marco Tulio Ribeiro <marcotcr@gmail.com>
2023-09-14 17:58:29 +0000

    llamachat tokens

--- guidance/endpoints/transformers/_llama.py
+++ guidance/endpoints/transformers/_llama.py
@@ -16,3 +16,25 @@ class LLaMA(Transformers):
             model = transformers.LlamaForCausalLM.from_pretrained(model, **kwargs)
             
         return super()._model_and_tokenizer(model, tokenizer, **kwargs)
+
+class LLaMAChat(LLaMA):
+
+    default_system_prompt = """You are a helpful assistant."""
+
+    # https://github.com/facebookresearch/llama/blob/main/llama/generation.py#L45
+    # This is not exatly right, I think they do a single [INST] for the system prompt AND first instruction
+    @staticmethod
+    def role_start(role):
+       return  {
+        'system': '[INST] <<SYS>>\n',
+        'user': '[INST] ',
+        'assistant': '',
+        }[role]
+    
+    @staticmethod
+    def role_end(role):
+       return  {
+        'system': '\n<</SYS>>\n\n[/INST]',
+        'user': '[/INST]',
+        'assistant': '',
+        }[role]
