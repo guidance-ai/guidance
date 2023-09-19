42fb4f5043e4106a5b3fbebec9a04362a768f45a
Scott Lundberg <scott@scottlundberg.com>
2023-09-19 17:35:58 +0000

    Allow silent stuff to get shown

--- guidance/library/_silent.py
+++ guidance/library/_silent.py
@@ -2,6 +2,6 @@ import guidance
 
 @guidance
 def silent(self, **kwargs):
-    open_text = f"<||_html:<div style='display: none;'>_||>"
+    open_text = f"""<||_html:<div style='display: inline-block; cursor: pointer; opacity: 0.5; width: 2px; margin-left: -2px;' onClick='this.nextSibling.style.display = "inline"; this.style.display = "none"'>&caron;</div><div style='display: none;'>_||>"""
     close_text = "<||_html:</div>_||>"
     return self.block(open_text=open_text, close_text=close_text)
