0d17070d39b40fb3cdab0472a48ad464c8d83b90
Marco Tulio Ribeiro <marcotcr@gmail.com>
2023-09-26 21:47:41 +0000

    made lm.eos optional, but it's still wrong

--- guidance/library/_gen.py
+++ guidance/library/_gen.py
@@ -49,7 +49,7 @@ def gen(lm, name=None, *, max_tokens=1000, list_append=False, pattern=None, stop
         stop_regex = ""
     else:
         if stop_regex is None:
-            stop_regex = [regex.escape(lm.eos_token)]
+            stop_regex = ['('+ regex.escape(lm.eos_token) + ')?']
         stop_regex = "(?P<stop>" + "|".join(stop_regex) + ")"
 
     pattern += stop_regex
