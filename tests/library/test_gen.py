f963f59402a5b07282fab0ecf38f8a762737e191
Marco Tulio Ribeiro <marcotcr@gmail.com>
2023-09-28 17:02:08 +0000

    gsm8k test

--- tests/library/test_gen.py
+++ tests/library/test_gen.py
@@ -19,3 +19,8 @@ def test_unicode():
     Step 1''' + gen('steps', list_append=True, stop=['\nStep', '\n\n', '\nAnswer'], temperature=0.7, max_tokens=20) + '\n'
     i = 2
     lm + f'Step {i}:' + gen('steps', list_append=True, stop=['\nStep', '\n\n', '\nAnswer'], temperature=0.7, max_tokens=20) + '\n'
+
+def test_gsm8k():
+    lm = get_model("transformers:gpt2")
+    lm + '''Question: Janet’s ducks lay 16 eggs per day. She eats three for breakfast every morning and bakes muffins for her friends every day with four. She sells the remainder at the farmers' market daily for $2 per fresh duck egg. How much in dollars does she make every day at the farmers' market?
+Answer: ''' + gen(max_tokens=30)
