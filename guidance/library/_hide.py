200ad5fcc9993a2e44fa6f577b5628fcefdf5a45
Scott Lundberg <scott@scottlundberg.com>
2023-10-10 21:18:24 +0000

    Fix nested hide

--- guidance/library/_hide.py
+++ guidance/library/_hide.py
@@ -1,3 +1,9 @@
 def hide(value):
-    value.hidden = True
+    _rec_hide(value)
     return value
+
+def _rec_hide(grammar):
+    grammar.hidden = True
+    if hasattr(grammar, "values"):
+        for g in grammar.values:
+            _rec_hide(g)
