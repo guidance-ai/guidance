19c57970ba86101aa89d68ac8483633b9f8af78c
Marco Tulio Ribeiro <marcotcr@gmail.com>
2023-10-24 19:48:30 +0000

    select tests

--- tests/library/test_select.py
+++ tests/library/test_select.py
@@ -5,3 +5,14 @@ def test_reset_pos():
     model = models.LocalMock()
     model += 'This is' + select(values=['bad', 'quite bad'])
     assert str(model) in ["This isbad", "This isquite bad"]
+
+def test_simple():
+    lm = get_model("transformers:gpt2")
+    options = ['baad I think', 'bad I think', 'bad']
+    lm = lm + 'Scott is quite ' + select(name='bad', values=options)
+    assert lm['bad'] in options
+
+def test_longer_select():
+    lm = models.LocalMock(b"<s>Scott is a very nice man.")
+    lm += "Scott is a very " + select(name='text', values=['nice', 'nice man.'])
+    assert lm["text"] == "nice man."
