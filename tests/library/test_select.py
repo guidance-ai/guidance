8a6e611f180c56a53be7dd29ab920f022ac7bc0a
Scott Lundberg <scott@scottlundberg.com>
2023-10-27 00:42:30 +0000

    rename values to options in select

--- tests/library/test_select.py
+++ tests/library/test_select.py
@@ -3,17 +3,17 @@ from ..utils import get_model
 
 def test_reset_pos():
     model = models.LocalMock()
-    model += 'This is' + select(values=['bad', 'quite bad'])
+    model += 'This is' + select(options=['bad', 'quite bad'])
     assert str(model) in ["This isbad", "This isquite bad"]
 
 def test_simple():
     lm = get_model("transformers:gpt2")
     options = ['baad I think', 'bad I think', 'bad']
-    lm = lm + 'Scott is quite ' + select(name='bad', values=options)
+    lm = lm + 'Scott is quite ' + select(name='bad', options=options)
     assert lm['bad'] in options
 
 def test_longer_select():
     '''This tests to ensure that the grammar is extended greedily.'''
     lm = models.LocalMock(b"<s>Scott is a very nice man.")
-    lm += "Scott is a very " + select(name='text', values=['nice', 'nice man.'])
+    lm += "Scott is a very " + select(name='text', options=['nice', 'nice man.'])
     assert lm["text"] == 'nice man.'
