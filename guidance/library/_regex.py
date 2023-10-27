4c04ad0fe5c996e2caf3207ebe2639bf5de5c576
Marco Tulio Ribeiro <marcotcr@gmail.com>
2023-10-27 22:23:07 +0000

    fixing bugs in regex

--- guidance/library/_regex.py
+++ guidance/library/_regex.py
@@ -4,14 +4,19 @@ from ._string import string
 from ._zero_or_more import zero_or_more
 from .._grammar import Byte
 from ._select import select
+import re
 from types import SimpleNamespace
 from pyformlang.regular_expression import PythonRegex
 
 
 @guidance(stateless=True, dedent=False)
 def regex(lm, pattern):
+    # find all of the brackets we'll need to negate later
+    nots = re.findall('\[\^(.*?)\]', pattern)
+    # Compensating for a weird design choice in pyformlang where they don't accept \n in .
+    pattern = re.sub('\.', '(.|\n)', pattern)
     regex = PythonRegex(pattern)
-    cfg = tree_to_grammar(simplify_tree(regex))
+    cfg = tree_to_grammar(simplify_tree(regex), nots=nots)
     return cfg
 
 
@@ -39,9 +44,17 @@ class FakeNode:
         self.head = SimpleNamespace()
         self.head.value = value
         self.sons = sons
+    def get_tree_str(self, depth: int = 0) -> str:
+        """ Get a string representation of the tree behind the regex
+        """
+        temp = " " * depth + str(self.head) + "\n"
+        for son in self.sons:
+            temp += son.get_tree_str(depth + 1)
+        return temp
+
 
 # 
-def tree_to_grammar(node):
+def tree_to_grammar(node, nots):
     """Takes a pyformlang regex tree and returns a guidance gramar
     """
     if not node.sons:
@@ -52,19 +65,21 @@ def tree_to_grammar(node):
             val = string(val)
         return val
     if node.head.value == 'Union':
-        vals = [tree_to_grammar(x) for x in node.sons]
-        # If select starts with ^ and is a select between bytes, negate it. The way I implemented it is a major hack, should fix
+        vals = [tree_to_grammar(x, nots) for x in node.sons]
+        # If select starts with ^ and is a select between bytes and it is listed in our negations, negate it. The way I implemented it is a major hack, should fix
         if all([isinstance(x, Byte) for x in  vals]) and vals[0].byte == b'^':
-            vals = [x.byte for x in vals[1:]]
-            return select([byte_range(x[0], x[1]) for x in negative_byte_range(vals)])
+            temp_vals = [x.byte for x in vals[1:]]
+            if b''.join(temp_vals).decode('utf8') in nots:
+                # print('Negating', temp_vals)
+                return select([byte_range(x[0], x[1]) for x in negative_byte_range(temp_vals)])
         return select(vals)
     if node.head.value == 'Concatenation':
         ret = ''
         for x in node.sons:
-            ret += tree_to_grammar(x)
+            ret += tree_to_grammar(x, nots)
         return ret
     if node.head.value == 'Kleene Star':
-        return zero_or_more(tree_to_grammar(node.sons[0]))
+        return zero_or_more(tree_to_grammar(node.sons[0], nots))
 
 
 def simplify_tree(regex):
@@ -86,9 +101,9 @@ def merge_nodes(regex, op):
         while len(current.sons[0].sons) == 0 and len(current.sons[1].sons) and current.sons[1].head.value == op:
             val.append(current.sons[0].head.value)
             current = current.sons[1]
-        if len(current.sons[0].sons) == 0 and len(current.sons[1].sons) == 0 and current.head.value == op:
-            val.append(current.sons[0].head.value)
-            val.append(current.sons[1].head.value)
+        # if all I have is leaves, I can group them
+        if all([len(x.sons) == 0 for x in current.sons]) and current.head.value == op:
+            val.extend([x.head.value for x in current.sons])
             if op == 'Concatenation':
                 val = ''.join(val)
                 new_node = FakeNode(val, [])
