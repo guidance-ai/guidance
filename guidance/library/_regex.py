2799f39d5e74963082ede885fb7820265fb6fd9d
Marco Tulio Ribeiro <marcotcr@gmail.com>
2023-10-31 16:15:24 +0000

    Fixed bugs in regex

--- guidance/library/_regex.py
+++ guidance/library/_regex.py
@@ -13,6 +13,7 @@ from pyformlang.regular_expression import PythonRegex
 def regex(lm, pattern):
     # find all of the brackets we'll need to negate later
     nots = re.findall('\[\^(.*?)\]', pattern)
+    nots = [re.compile('[' + x + ']') for x in nots]
     # Compensating for a weird design choice in pyformlang where they don't accept \n in .
     pattern = re.sub(r'(?<!\\)\.', '(.|\n)', pattern)
     regex = PythonRegex(pattern)
@@ -69,9 +70,16 @@ def tree_to_grammar(node, nots):
         # If select starts with ^ and is a select between bytes and it is listed in our negations, negate it. The way I implemented it is a major hack, should fix
         if all([isinstance(x, Byte) for x in  vals]) and vals[0].byte == b'^':
             temp_vals = [x.byte for x in vals[1:]]
-            if b''.join(temp_vals).decode('utf8') in nots:
-                # print('Negating', temp_vals)
-                return select([byte_range(x[0], x[1]) for x in negative_byte_range(temp_vals)])
+            all_chars = b''.join(temp_vals).decode('utf8')
+            for notz in nots:
+                # The not matches all of the chars.
+                # TODO: Technically there could be a situation where this is true AND this is not actually what we had in a regex, so this code is wrong. But it's probably never going to happen, and we'll replace this code later.
+                if all([notz.match(x) is not None for x in all_chars]):
+                    return select([byte_range(x[0], x[1]) for x in negative_byte_range(temp_vals)])
+
+            # if b''.join(temp_vals).decode('utf8') in nots:
+            #     # print('Negating', temp_vals)
+            #     return select([byte_range(x[0], x[1]) for x in negative_byte_range(temp_vals)])
         return select(vals)
     if node.head.value == 'Concatenation':
         ret = ''
@@ -98,9 +106,16 @@ def merge_nodes(regex, op):
     current = regex
     if len(current.sons) and current.head.value == op:
         val = []
-        while len(current.sons[0].sons) == 0 and len(current.sons[1].sons) and current.sons[1].head.value == op:
-            val.append(current.sons[0].head.value)
-            current = current.sons[1]
+        while (len(current.sons[0].sons) == 0 and len(current.sons[1].sons) and current.sons[1].head.value == op) or (
+               len(current.sons[1].sons) == 0 and len(current.sons[0].sons) and current.sons[0].head.value == op):
+            if len(current.sons[0].sons) == 0:
+                leaf = current.sons[0]
+                other = current.sons[1]
+            else:
+                leaf = current.sons[1]
+                other = current.sons[0]
+            val.append(leaf.head.value)
+            current = other
         # if all I have is leaves, I can group them
         if all([len(x.sons) == 0 for x in current.sons]) and current.head.value == op:
             val.extend([x.head.value for x in current.sons])
@@ -120,4 +135,4 @@ def merge_nodes(regex, op):
             new_node = FakeNode(op, [FakeNode('Union', [FakeNode(x, []) for x in val]), merge_nodes(current, op)])
         return new_node
     else:
-        return current
+        return regex
