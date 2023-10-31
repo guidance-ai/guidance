49d5f4d311109d8ce5875d1d45c81aadbfa2f957
Marco Tulio Ribeiro <marcotcr@gmail.com>
2023-10-31 17:51:42 +0000

    long prompts don't work

--- tests/library/test_gen.py
+++ tests/library/test_gen.py
@@ -120,3 +120,52 @@ def test_various_regexes():
 #     lm = get_model("transformers:gpt2")
 #     lm += 'hey there my friend what is truth 23+43=' + gen(pattern=r'dog(?P<stop>.+)', max_tokens=30)
 #     assert str(lm) == "hey there my friend what is truth 23+43=dog"
+
+def test_long_prompt():
+    lm = get_model("transformers:gpt2")
+    prompt = '''Question: Legoland has 5 kangaroos for each koala. If Legoland has 180 kangaroos, how many koalas and kangaroos are there altogether?
+Let's think step by step, and then write the answer:
+Step 1: For every 5 kangaroos, there is one koala, meaning for the 180 kangaroos, there are 180/5 = 36 koalas.
+Step 2: Altogether, there are 36+180 = 216 koalas and kangaroos.
+Answer: 216
+
+Question: Jennifer has ten pears, 20 oranges, and twice as many apples as pears. If she gives her sister two of each fruit, how many fruits does she have left?
+Let's think step by step, and then write the answer:
+Step 1: Jennifer has 2*10 = 20 apples.
+Step 2: She has a total of 10+20+20 = 50 fruits.
+Step 3: She gives her sister 2 pears+2 oranges+2 apples = 2+2+2 = 6 fruits.
+Step 4: After giving her sister 2 of each fruit, she has 50-6 = 44 fruits left.
+
+Answer: 44
+
+Question: A busy restaurant is counting how many customers they had during that Friday to try to predict how many they might get on Saturday. During breakfast, they had 73 customers. During lunch, they had 127 customers. During dinner, they had 87 customers. If they predict they'll get twice the amount of customers on Saturday as they had on Friday, how many customers do they predict they will get?
+Let's think step by step, and then write the answer:
+Step 1: On Friday the restaurant had 73 customers for breakfast + 127 customers for lunch + 87 customers for dinner = 73+127+87 = 287 customers total on Friday.
+Step 2: If they predict getting 2x the amount of customers on Saturday as on Friday, they will have 287 customers x 2 = 287*2 = 574 customers on Saturday.
+
+Answer: 574
+
+Question: They say the first year of a dog's life equals 15 human years. The second year of a dog's life equals 9 human years and after that, every year of a dog's life equals 5 human years. According to this logic, how many human years has my 10-year-old dog lived?
+Let's think step by step, and then write the answer:
+Step 1: If your dog is 10 years old then in his first year of life he lived 1*15 = 15 human years
+Step 2: In his second year of life, he lived 1*9 = 9 human years
+Step 3: We need to calculate his remaining years or 10-2 = 8 years of dog life into human years
+Step 4: If 1 year of dog life after the 2 years equates to 5 human years, then 8 years of dog life equals 8*5 = 40 human years
+Step 5: In total, your dog has lived 15 + 9 + 40 = 64 human years
+
+Answer: 64
+
+Question: Amanda and her family are going to re-paint all the walls inside their house. Before they get started they want to divide up the work. Since all the rooms in the house have different numbers and sizes of walls in them, they figure the fairest way to divide up the work is to count all the walls in the house and assign an equal number to each person. There are 5 people in Amanda's family, including herself. There are 9 rooms in the house. 5 of the rooms have 4 walls each. The other 4 rooms each have 5 walls each. To be fair, how many walls should each person in Amanda's family paint?
+Let's think step by step, and then write the answer:
+Step 1: First, Amanda needs to figure out how many walls there are in the house, 5 rooms x 4 walls each = 5*4 = 20.
+Step 2: The other 4 rooms have x 5 walls each = 4 * 5 = 20 walls.
+Step 3: The house has 20 walls + 20 walls = 20 + 20 = 40 walls total.
+Step 4: To divide the work equally between the 5 people, 40 walls / 5 people = 40/5 = 8 walls for each person.
+
+Answer: 8
+
+Question: Janet’s ducks lay 16 eggs per day. She eats three for breakfast every morning and bakes muffins for her friends every day with four. She sells the remainder at the farmers' market daily for $2 per fresh duck egg. How much in dollars does she make every day at the farmers' market?
+Let's think step by step, and then write the answer:
+Step 1'''
+    lm +=  prompt + gen(max_tokens=100)
+    assert True
