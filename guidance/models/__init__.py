4130ccc8dac79be1b65d4b6a14cb6b7455f740e5
Daniel E Fredriksen <daniel.fredriksen@us.kbr.com>
2023-11-29 22:30:52 -0500

    Dont forget to add these classes back into the import statement

--- guidance/models/__init__.py
+++ guidance/models/__init__.py
@@ -1,6 +1,6 @@
 from ._model import Model, Chat
 from .vertexai._vertexai import VertexAI, VertexAIChat
-from ._azure_openai import AzureOpenAI, AzureOpenAIChat
+from ._azure_openai import AzureOpenAI, AzureOpenAIChat, AzureOpenAIInstruct, AzureOpenAICompletion
 from ._openai import OpenAI, OpenAIChat, OpenAIInstruct, OpenAICompletion
 from .transformers._transformers import Transformers, TransformersChat
 from ._llama_cpp import LlamaCpp, LlamaCppChat
