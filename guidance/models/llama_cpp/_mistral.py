8e8c438e5664d2a2887b9c3fb2737c82efa62056
Bono Lv <lvscar@gmail.com>
2024-01-05 11:45:15 +0800

    add openorca format

--- guidance/models/llama_cpp/_mistral.py
+++ guidance/models/llama_cpp/_mistral.py
@@ -1,5 +1,5 @@
 from .._model import Chat, Instruct
-from ._llama_cpp import LlamaCpp
+from ._llama_cpp import LlamaCpp , LlamaCppChat
 
 class MistralChat(LlamaCpp, Chat):
     def get_role_start(self, role_name, **kwargs):
@@ -28,3 +28,18 @@ class MistralInstruct(LlamaCpp, Instruct):
     def get_role_end(self, role_name=None):
         if role_name == "instruction":
             return " [/INST]"
+
+
+class MistralChatOpenOrca(LlamaCppChat, Chat):
+    def get_role_start(self, role_name, **kwargs):
+        if role_name == "user":
+            return "<|im_start|>user\n"
+        
+        elif role_name == "assistant":
+            return "<|im_start|>assistant\n"
+        
+        elif role_name == "system":
+            return "<|im_start|>system\n"            
+    
+    def get_role_end(self, role_name=None):
+        return "<|im_end|>\n"
