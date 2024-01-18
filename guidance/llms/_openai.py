20d3a07a84ed425a19f9dd710412933a7b48202f
wille <mxyyzm@gmail.com>
2024-01-18 21:52:58 +0800

    Add preview models (#7)

--- guidance/llms/_openai.py
+++ guidance/llms/_openai.py
@@ -70,7 +70,8 @@ def prompt_to_messages(prompt):
 
 async def add_text_to_chat_mode_generator(chat_mode):
     in_function_call = False
-    async for resp in chat_mode:
+    async for part in chat_mode:
+        resp = part.model_dump()
         if "choices" in resp:
             for c in resp['choices']:
 
@@ -114,6 +115,7 @@ def add_text_to_chat_mode(chat_mode):
     if isinstance(chat_mode, (types.AsyncGeneratorType, types.GeneratorType)):
         return add_text_to_chat_mode_generator(chat_mode)
     else:
+        chat_mode = chat_mode.model_dump()
         for c in chat_mode['choices']:
             c['text'] = c['message']['content']
         return chat_mode
@@ -154,7 +156,7 @@ class OpenAI(LLM):
         # auto detect chat completion mode
         if chat_mode == "auto":
             # parse to determin if the model need to use the chat completion API
-            chat_model_pattern = r'^(gpt-3\.5-turbo|gpt-4)(-\d+k)?(-\d{4})?$'
+            chat_model_pattern = r'^(gpt-3\.5-turbo|gpt-4)(-\d+k)?(-\d{4})?(-preview)?$'
             if re.match(chat_model_pattern, model):
                 chat_mode = True
             else:
@@ -380,6 +382,7 @@ class OpenAI(LLM):
             out = add_text_to_chat_mode(out)
         else:
             out = await client.completions.create(**kwargs)
+            out = out.model_dump()
 
         # restore the params of the openai library
         openai.api_key = prev_key
