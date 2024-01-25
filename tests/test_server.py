12c721d55aff0e39a8d3e9cf93413633c198bb16
Scott Lundberg <scott@scottlundberg.com>
2024-01-25 20:57:35 +0000

    Use Mock for server test

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,6 +1,3 @@
-import guidance
-from guidance import models, select, gen
-from .utils import get_model
 import pytest
 import multiprocessing
 import time
@@ -8,7 +5,7 @@ import time
 def server_process():
     from guidance import models, Server
 
-    mistral = get_model("llama_cpp:")
+    mistral = models.Mock()
 
     server = Server(mistral, api_key="SDFSDF")
     server.run(port=8392)
@@ -24,6 +21,6 @@ def running_server():
 def test_remote_llama_cpp_gen(running_server):
     from guidance import models, gen
 
-    m = models.LlamaCpp("http://localhost:8392", api_key="SDFSDF")
+    m = models.Mock("http://localhost:8392", api_key="SDFSDF")
     m2 = m + "A story." + gen("test", max_tokens=20)
     assert len(str(m2)) > 20, "The model didn't generate enough data"
