aca0564216cb57fdd106bf4640aefd22f8fcdb9b
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2024-02-08 17:17:04 -0500

    linting

--- guidance/_json_schema_to_grammar.py
+++ guidance/_json_schema_to_grammar.py
@@ -1,7 +1,7 @@
 import json
 from typing import Dict
 
-from ._grammar import Byte, GrammarFunction, Join, select, Select
+from ._grammar import Byte, GrammarFunction, Join, Select, select
 from .library._char_range import char_range
 
 _QUOTE = Byte(b'"')
