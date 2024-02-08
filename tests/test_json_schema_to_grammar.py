63d2174fa5b6b347419b0012cd1a43735ce30079
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2024-02-08 09:56:50 -0500

    Correcting test a little

--- tests/test_json_schema_to_grammar.py
+++ tests/test_json_schema_to_grammar.py
@@ -1,3 +1,7 @@
+import json
+
+from jsonschema import validate
+
 from guidance._parser import EarleyCommitParser
 from guidance._grammar import Byte
 
@@ -7,15 +11,17 @@ from guidance._json_schema_to_grammar import json_schema_to_grammar
 def test_string_schema():
     schema = """{ "type": "string" }"""
 
-    grammar = json_schema_to_grammar(schema)
-    parser = EarleyCommitParser(grammar)
-
     simple_json_string = '"my_string"'
 
-    simple_json_string_bytes = bytes(simple_json_string, encoding="utf8")
+    # First sanity check what we're setting up
+    schema_obj = json.loads(schema)
+    validate(instance=simple_json_string, schema=schema_obj)
+
+    # Now set up the actual conversion
+    grammar = json_schema_to_grammar(schema)
+    parser = EarleyCommitParser(grammar)
 
     for c in simple_json_string:
         print(f"Working on: {c}")
         next_byte = Byte(bytes(c, encoding="utf8"))
-        assert parser.valid_next_bytes() == set([next_byte])
         parser.consume_byte(next_byte)
