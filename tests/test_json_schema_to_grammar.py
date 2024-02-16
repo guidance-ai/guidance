59a5804ab111c3c1be69a59fd20f70817641a683
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2024-02-16 09:16:24 -0500

    Get the test to fail

--- tests/test_json_schema_to_grammar.py
+++ tests/test_json_schema_to_grammar.py
@@ -353,6 +353,11 @@ def test_simple_ref(target_obj):
     # First sanity check what we're setting up
     schema_obj = json.loads(schema)
     validate(instance=target_obj, schema=schema_obj)
+    
+    grammar = json_schema_to_grammar(schema)
+
+    target_string = to_compact_json(schema_obj)
+    check_string_with_grammar(target_string, grammar)
 
 
 def test_with_mock_model():
