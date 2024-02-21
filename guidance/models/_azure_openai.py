e6b9ad468bc6a983dbcd7baabebb83e2a4d41ba6
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2024-02-21 11:13:41 -0500

    Take fixes

--- guidance/models/_azure_openai.py
+++ guidance/models/_azure_openai.py
@@ -1,25 +1,26 @@
 import re
-from urllib.parse import urlparse, parse_qs
+from urllib.parse import parse_qs, urlparse
 
+from ._grammarless import Grammarless
+from ._model import Chat, Instruct
 from ._openai import (
-    OpenAIChat,
-    OpenAICompletion,
-    OpenAIInstruct,
+    OpenAIChatEngine,
+    OpenAICompletionEngine,
+    OpenAIEngine,
+    OpenAIInstructEngine,
     chat_model_pattern,
 )
-from ._openai import OpenAI
 
 try:
     # TODO: can we eliminate the torch requirement for llama.cpp by using numpy in the caller instead?
     import openai as openai_package
-    import tiktoken
 
     is_openai = True
 except ImportError:
     is_openai = False
 
 
-class AzureOpenAI(OpenAI):
+class AzureOpenAI(Grammarless):
     def __init__(
         self,
         model: str,
@@ -29,22 +30,21 @@ class AzureOpenAI(OpenAI):
         api_key: str = None,
         tokenizer=None,
         echo=True,
-        # caching=True,
-        # temperature=0.0,
-        # top_p=1.0,
-        # max_streaming_tokens=500,
-        version="2023-10-01-preview",
+        version: str = None,
+        max_streaming_tokens=1000,
+        timeout=0.5,
+        compute_log_probs=False,
         **kwargs,
     ):
-        '''Build a new AzureOpenAI model object that represents a model in a given state.'''
+        """Build a new AzureOpenAI model object that represents a model in a given state."""
         if not is_openai or not hasattr(openai_package, "OpenAI"):
-            raise Exception(
+            raise ImportError(
                 "Please install the openai package version >= 1 using `pip install openai -U` "
                 "in order to use guidance.models.OpenAI!"
             )
 
         if api_key is None and azure_ad_token_provider is None:
-            raise Exception("Please provide either api_key or azure_ad_token_provider")
+            raise ValueError("Please provide either api_key or azure_ad_token_provider")
 
         # if we are called directly (as opposed to through super()) then we convert ourselves to
         # a more specific subclass if possible
@@ -77,11 +77,7 @@ class AzureOpenAI(OpenAI):
                 azure_deployment=azure_deployment,
                 tokenizer=tokenizer,
                 echo=echo,
-                # caching=caching,
-                # temperature=temperature,
-                # max_streaming_tokens=max_streaming_tokens,
                 version=version,
-                # top_p=top_p,
                 **kwargs,
             )
             return
@@ -93,39 +89,50 @@ class AzureOpenAI(OpenAI):
             if "api-version" not in parsed_query
             else parsed_query["api-version"]
         )
+        engine_map = {
+            AzureOpenAICompletion: OpenAICompletionEngine,
+            AzureOpenAIChat: OpenAIChatEngine,
+            AzureOpenAIInstruct: OpenAIInstructEngine,
+        }
+        engine_class = engine_map[self.__class__]
 
-        self.client = openai_package.AzureOpenAI(
+        engine_instance = engine_class(
+            tokenizer=tokenizer,
+            max_streaming_tokens=max_streaming_tokens,
+            timeout=timeout,
+            compute_log_probs=compute_log_probs,
+            model=model,
             azure_endpoint=azure_endpoint,
             api_key=api_key,
             azure_ad_token_provider=azure_ad_token_provider,
             api_version=api_version,
-            azure_deployment=azure_deployment
-        )
-        self.model_name = model
-        # self.top_p = top_p
-
-        super().__init__(
-            model=azure_endpoint,
-            tokenizer=tokenizer or tiktoken.encoding_for_model(model),
-            echo=echo,
-            # caching=caching,
-            # temperature=temperature,
-            # top_p=top_p,
-            # max_streaming_tokens=max_streaming_tokens,
+            azure_deployment=azure_deployment,
+            client_class=openai_package.AzureOpenAI,
             **kwargs,
         )
 
-
-class AzureOpenAIChat(AzureOpenAI, OpenAIChat):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-
-class AzureOpenAIInstruct(AzureOpenAI, OpenAIInstruct):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        super().__init__(
+            engine_instance,
+            echo=echo,
+        )
 
 
-class AzureOpenAICompletion(AzureOpenAI, OpenAICompletion):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+class AzureOpenAIChat(AzureOpenAI, Chat):
+    pass
+
+
+class AzureOpenAICompletion(AzureOpenAI):
+    pass
+
+
+class AzureOpenAIInstruct(AzureOpenAI, Instruct):
+    def get_role_start(self, name):
+        return ""
+
+    def get_role_end(self, name):
+        if name == "instruction":
+            return "<|endofprompt|>"
+        else:
+            raise ValueError(
+                f"The OpenAIInstruct model does not know about the {name} role type!"
+            )
