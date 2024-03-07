908e8a92637fb1a622b2e3b102f3bafc121d203d
HelloWorldLTY <43333475+HelloWorldLTY@users.noreply.github.com>
2024-03-07 13:30:30 -0800

    Update test_peft.py

--- tests/models/test_peft.py
+++ tests/models/test_peft.py
@@ -1,8 +1,22 @@
-import guidance
-import pytest
-
+from transformers import AutoModelForCausalLM
 def test_peft():
     try:
         import peft
+        from peft import get_peft_model
+        
+        lora_config = LoraConfig(
+            r=16,
+            target_modules=["q_proj", "v_proj"],
+            task_type=TaskType.CAUSAL_LM,
+            lora_alpha=32,
+            lora_dropout=0.05
+        )
+        model = AutoModelForCausalLM.from_pretrained("facebook/opt-350m")
+        
+        lora_model = get_peft_model(model, lora_config)
+        lora_model.print_trainable_parameters()
+
+        print("Running PEFT is successful!")
+    
     except:
         raise Exception("Sorry, peft is not installed")
