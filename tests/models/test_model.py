12e6f7773951365e72a8ee522f6d9be8032d9454
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2024-03-14 15:05:18 -0400

    Fix for GPT2 bug test

--- tests/models/test_model.py
+++ tests/models/test_model.py
@@ -43,6 +43,10 @@ def test_call_embeddings():
 
 def test_token_healing(selected_model):
     '''Tests a bug where the space is incorrectly forced as token 220, while it should be not forced it might be extended'''
+    model_type = type(selected_model.engine.model_obj).__name__
+    print(model_type)
+    if model_type != "GPT2LMHeadModel":
+        pytest.skip("Test for GPT2 bug only")
     gpt2 = selected_model
     lm = gpt2 + ("This is a story of 10 or 5 or " + zero_or_more(byte_range(b'0', b'9')))
     assert len(lm) > len("This is a story of 10 or 5 or ")
