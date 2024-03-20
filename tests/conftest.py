83c7f190e11f97f0c649ae29ba2a813936247ff7
Harsha-Nori <harsha.nori@live.com>
2024-03-19 23:12:16 -0700

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,7 +11,8 @@ AVAILABLE_MODELS = {
     ),
     "hfllama7b": dict(
         name="huggingface_hubllama:TheBloke/Llama-2-7B-GGUF:llama-2-7b.Q5_K_M.gguf",
-        kwargs=dict(),
+        kwargs=dict()
+    ),
     "gpt2gpu": dict(name="transformers:gpt2", kwargs={"device_map": "cuda:0"}),
     "phi2gpu": dict(
         name="transformers:microsoft/phi-2",
