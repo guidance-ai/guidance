19c92814a37967936de3b53f4a3e1bf06903c782
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2024-03-20 13:48:13 -0400

    Correct

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,6 +9,10 @@ AVAILABLE_MODELS = {
     "phi2cpu": dict(
         name="transformers:microsoft/phi-2", kwargs={"trust_remote_code": True}
     ),
+    "hfllama7b": dict(
+        name="huggingface_hubllama:TheBloke/Llama-2-7B-GGUF:llama-2-7b.Q5_K_M.gguf",
+        kwargs={"verbose": True},
+    ),
     "gpt2gpu": dict(name="transformers:gpt2", kwargs={"device_map": "cuda:0"}),
     "phi2gpu": dict(
         name="transformers:microsoft/phi-2",
@@ -28,7 +32,12 @@ def pytest_addoption(parser):
 
 
 @pytest.fixture(scope="session")
-def selected_model(pytestconfig) -> models.Model:
+def selected_model_name(pytestconfig) -> str:
+    return pytestconfig.getoption("selected_model")
+
+
+@pytest.fixture(scope="session")
+def selected_model(selected_model_name: str) -> models.Model:
     """Get a concrete model for tests
 
     This fixture is for tests which are supposed
@@ -41,9 +50,7 @@ def selected_model(pytestconfig) -> models.Model:
     controlled by the '--selected_model' command
     line argument to pytest.
     """
-    model_key = pytestconfig.getoption("selected_model")
-
-    model_info = AVAILABLE_MODELS[model_key]
+    model_info = AVAILABLE_MODELS[selected_model_name]
 
     model = get_model(model_info["name"], **(model_info["kwargs"]))
     return model
