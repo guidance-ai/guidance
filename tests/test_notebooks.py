7e587ec914ac7cbfc7eb4c794d9f69c9e2ea8ad1
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2024-04-05 13:42:17 -0400

    Do need to use the GPU

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -49,6 +49,7 @@ class TestArtOfPromptDesign:
     BASE_APD_PATH = BASE_NB_PATH / "art_of_prompt_design"
 
     # @pytest.mark.skip(reason="Having trouble running")
+    @pytest.mark.use_gpu
     def test_prompt_boundaries_and_token_healing(self):
         nb_path = (
             TestArtOfPromptDesign.BASE_APD_PATH
