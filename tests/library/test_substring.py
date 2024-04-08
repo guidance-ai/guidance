9fb1c4bf151fc1d11c7ddf11e5cba6e9cce23b09
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2024-04-08 10:17:40 -0400

    Get test working

--- tests/library/test_substring.py
+++ tests/library/test_substring.py
@@ -3,8 +3,8 @@ from guidance import gen, models, commit_point, Tool, select, capture, string, s
 from ..utils import get_model
 import re
 
-def test_substring_equal_unconstrained():
-    llama2 = get_model("llama_cpp:")
-    lm = llama2 + 'ae galera ' + gen(max_tokens=10, name='test')
-    lm2 = llama2 + 'ae galera ' + substring(lm['test'])
+def test_substring_equal_unconstrained(selected_model):
+    target_model = selected_model
+    lm = target_model + 'ae galera ' + gen(max_tokens=10, name='test')
+    lm2 = target_model + 'ae galera ' + substring(lm['test'])
     assert str(lm) == str(lm2)
