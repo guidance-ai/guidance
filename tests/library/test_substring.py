3af988fea0911e66a69c5d5cc34b0ec04afc1b8c
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2024-04-08 10:18:25 -0400

    Tidy up

--- tests/library/test_substring.py
+++ tests/library/test_substring.py
@@ -1,10 +1,8 @@
-import guidance
-from guidance import gen, models, commit_point, Tool, select, capture, string, substring
-from ..utils import get_model
-import re
+from guidance import gen, substring
+
 
 def test_substring_equal_unconstrained(selected_model):
     target_model = selected_model
-    lm = target_model + 'ae galera ' + gen(max_tokens=10, name='test')
-    lm2 = target_model + 'ae galera ' + substring(lm['test'])
+    lm = target_model + "ae galera " + gen(max_tokens=10, name="test")
+    lm2 = target_model + "ae galera " + substring(lm["test"])
     assert str(lm) == str(lm2)
