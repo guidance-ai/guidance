740215b188a08756520e66a952d0ee393001dd4a
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2024-04-11 15:00:32 -0400

    Possible bug in prefix_tree?

--- guidance/library/_prefix_tree.py
+++ guidance/library/_prefix_tree.py
@@ -6,6 +6,7 @@ from .._grammar import select, string
 
 @guidance(stateless=True, dedent=False)
 def prefix_tree(lm, strings, partial_matches=False, name: Optional[str] = None):
+    print(f"prefix_tree: {strings=}")
 
     if len(strings) == 0:
         return lm
@@ -17,6 +18,7 @@ def prefix_tree(lm, strings, partial_matches=False, name: Optional[str] = None):
             if s[0] not in char_groups:
                 char_groups[s[0]] = []
             char_groups[s[0]].append(s[1:])
+    print(f"prefix_tree: {strings}  {char_groups=}")
 
     # enable any empty followup if partial matches are allowed
     if partial_matches:
@@ -27,6 +29,11 @@ def prefix_tree(lm, strings, partial_matches=False, name: Optional[str] = None):
         string(k) + prefix_tree(v, partial_matches=partial_matches)
         for k, v in char_groups.items()
     ]
+    print(f"prefix_tree: {strings} {suboptions=}")
+
+    # We shouldn't call select if we don't have any choices
+    if len(suboptions)==0:
+        return lm
 
     return lm + guidance.capture(
         select(suboptions, skip_checks=True), name=name
