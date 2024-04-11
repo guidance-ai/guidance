ab633549c049bc2b79f28ec8c32d8f52c6d5e847
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2024-04-11 13:22:10 -0400

    Add capture semantics & blacken

--- guidance/library/_prefix_tree.py
+++ guidance/library/_prefix_tree.py
@@ -1,8 +1,11 @@
+from typing import Optional
+
 import guidance
 from .._grammar import select, string
 
+
 @guidance(stateless=True, dedent=False)
-def prefix_tree(lm, strings, partial_matches=False):
+def prefix_tree(lm, strings, partial_matches=False, name: Optional[str] = None):
 
     if len(strings) == 0:
         return lm
@@ -14,12 +17,17 @@ def prefix_tree(lm, strings, partial_matches=False):
             if s[0] not in char_groups:
                 char_groups[s[0]] = []
             char_groups[s[0]].append(s[1:])
-    
+
     # enable any empty followup if partial matches are allowed
     if partial_matches:
         char_groups[""] = []
-    
-    # recursively build the tree
-    suboptions = [string(k) + prefix_tree(v, partial_matches=partial_matches) for k,v in char_groups.items()]
 
-    return lm + select(suboptions, skip_checks=True) # we skip normal type checks for speed
+    # recursively build the tree
+    suboptions = [
+        string(k) + prefix_tree(v, partial_matches=partial_matches)
+        for k, v in char_groups.items()
+    ]
+
+    return lm + guidance.capture(
+        select(suboptions, skip_checks=True), name=name
+    )  # we skip normal type checks for speed
