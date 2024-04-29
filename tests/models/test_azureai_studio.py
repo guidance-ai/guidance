2be7f58c34fd1a650d73de0bf1d8933129c5d804
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2024-04-29 11:24:57 -0400

    Add LLama3

--- tests/models/test_azureai_studio.py
+++ tests/models/test_azureai_studio.py
@@ -63,3 +63,29 @@ def test_azureai_mistral_chat_smoke(rate_limiter):
 
     print(str(lm))
     assert len(lm["text"]) > 0
+
+
+def test_azureai_llama3_chat_smoke(rate_limiter):
+    azureai_studio_endpoint = env_or_fail("AZURE_AI_STUDIO_LLAMA3_CHAT_ENDPOINT")
+    azureai_studio_deployment = env_or_fail("AZURE_AI_STUDIO_LLAMA3_CHAT_DEPLOYMENT")
+    azureai_studio_key = env_or_fail("AZURE_AI_STUDIO_LLAMA3_CHAT_KEY")
+
+    lm = models.AzureAIStudioChat(
+        azureai_studio_endpoint=azureai_studio_endpoint,
+        azureai_studio_deployment=azureai_studio_deployment,
+        azureai_studio_key=azureai_studio_key,
+    )
+    assert isinstance(lm, models.AzureAIStudioChat)
+
+    with system():
+        lm += "You are a math wiz."
+
+    with user():
+        lm += "What is 1 + 1?"
+
+    with assistant():
+        lm += gen(max_tokens=10, name="text", temperature=0.5)
+        lm += "Pick a number: "
+
+    print(str(lm))
+    assert len(lm["text"]) > 0
