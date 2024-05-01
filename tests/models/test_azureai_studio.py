b68e9d716a252eb5bb6c2774ee6f2f1e7aaee773
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2024-05-01 10:18:28 -0400

    Expand Azure AI Studio testing

--- tests/models/test_azureai_studio.py
+++ tests/models/test_azureai_studio.py
@@ -11,10 +11,23 @@ from ..utils import env_or_fail
 pytestmark = pytest.mark.needs_credentials
 
 
-def test_azureai_phi3_chat_smoke(rate_limiter):
-    azureai_studio_endpoint = env_or_fail("AZURE_AI_STUDIO_PHI3_ENDPOINT")
-    azureai_studio_deployment = env_or_fail("AZURE_AI_STUDIO_PHI3_DEPLOYMENT")
-    azureai_studio_key = env_or_fail("AZURE_AI_STUDIO_PHI3_KEY")
+def _get_chat_model(model_name: str):
+    if model_name == "phi3":
+        azureai_studio_endpoint = env_or_fail("AZURE_AI_STUDIO_PHI3_ENDPOINT")
+        azureai_studio_deployment = env_or_fail("AZURE_AI_STUDIO_PHI3_DEPLOYMENT")
+        azureai_studio_key = env_or_fail("AZURE_AI_STUDIO_PHI3_KEY")
+    elif model_name == "mistral":
+        azureai_studio_endpoint = env_or_fail("AZURE_AI_STUDIO_MISTRAL_CHAT_ENDPOINT")
+        azureai_studio_deployment = env_or_fail(
+            "AZURE_AI_STUDIO_MISTRAL_CHAT_DEPLOYMENT"
+        )
+        azureai_studio_key = env_or_fail("AZURE_AI_STUDIO_MISTRAL_CHAT_KEY")
+    elif model_name == "llama3":
+        azureai_studio_endpoint = env_or_fail("AZURE_AI_STUDIO_LLAMA3_CHAT_ENDPOINT")
+        azureai_studio_deployment = env_or_fail(
+            "AZURE_AI_STUDIO_LLAMA3_CHAT_DEPLOYMENT"
+        )
+        azureai_studio_key = env_or_fail("AZURE_AI_STUDIO_LLAMA3_CHAT_KEY")
 
     lm = models.AzureAIStudioChat(
         azureai_studio_endpoint=azureai_studio_endpoint,
@@ -23,6 +36,12 @@ def test_azureai_phi3_chat_smoke(rate_limiter):
         clear_cache=True,
     )
     assert isinstance(lm, models.AzureAIStudioChat)
+    return lm
+
+
+@pytest.mark.parametrize("chat_model_name", ["phi3", "llama3"])
+def test_azureai_chat_smoke(rate_limiter, chat_model_name: str):
+    lm = _get_chat_model(chat_model_name)
 
     with system():
         lm += "You are a math wiz."
@@ -40,18 +59,7 @@ def test_azureai_phi3_chat_smoke(rate_limiter):
 
 
 def test_azureai_mistral_chat_smoke(rate_limiter):
-    azureai_studio_endpoint = env_or_fail("AZURE_AI_STUDIO_MISTRAL_CHAT_ENDPOINT")
-    azureai_studio_deployment = env_or_fail("AZURE_AI_STUDIO_MISTRAL_CHAT_DEPLOYMENT")
-    azureai_studio_key = env_or_fail("AZURE_AI_STUDIO_MISTRAL_CHAT_KEY")
-
-    lm = models.AzureAIStudioChat(
-        azureai_studio_endpoint=azureai_studio_endpoint,
-        azureai_studio_deployment=azureai_studio_deployment,
-        azureai_studio_key=azureai_studio_key,
-        clear_cache=True,
-    )
-    assert isinstance(lm, models.AzureAIStudioChat)
-    lm.engine.cache.clear()
+    lm = _get_chat_model("mistral")
 
     # No "system" role for Mistral?
     # with system():
@@ -69,18 +77,37 @@ def test_azureai_mistral_chat_smoke(rate_limiter):
     assert str(lm).endswith("Pick a number: <|im_end|>")
 
 
-def test_azureai_llama3_chat_smoke(rate_limiter):
-    azureai_studio_endpoint = env_or_fail("AZURE_AI_STUDIO_LLAMA3_CHAT_ENDPOINT")
-    azureai_studio_deployment = env_or_fail("AZURE_AI_STUDIO_LLAMA3_CHAT_DEPLOYMENT")
-    azureai_studio_key = env_or_fail("AZURE_AI_STUDIO_LLAMA3_CHAT_KEY")
+@pytest.mark.parametrize("chat_model_name", ["phi3", "llama3"])
+def test_azureai_chat_longer_1(rate_limiter, chat_model_name: str):
+    lm = _get_chat_model(chat_model_name)
 
-    lm = models.AzureAIStudioChat(
-        azureai_studio_endpoint=azureai_studio_endpoint,
-        azureai_studio_deployment=azureai_studio_deployment,
-        azureai_studio_key=azureai_studio_key,
-        clear_cache=True,
-    )
-    assert isinstance(lm, models.AzureAIStudioChat)
+    with system():
+        lm += "You are a math wiz."
+
+    with user():
+        lm += "What is 1 + 1?"
+
+    with assistant():
+        lm += gen(max_tokens=10, name="text")
+        lm += "Pick a number: "
+
+    print(str(lm))
+    assert len(lm["text"]) > 0
+    assert str(lm).endswith("Pick a number: <|im_end|>")
+
+    with user():
+        lm += "10. Now you pick a number between 0 and 20"
+
+    with assistant():
+        lm += gen(max_tokens=2, name="number")
+
+    print(str(lm))
+    assert len(lm["number"]) > 0
+
+
+@pytest.mark.parametrize("chat_model_name", ["phi3", "llama3"])
+def test_azureai_chat_longer_2(rate_limiter, chat_model_name: str):
+    lm = _get_chat_model(chat_model_name)
 
     with system():
         lm += "You are a math wiz."
@@ -90,22 +117,23 @@ def test_azureai_llama3_chat_smoke(rate_limiter):
 
     with assistant():
         lm += "2"
-    
+
     with user():
         lm += "What is 2 + 3?"
 
     with assistant():
-        lm += gen(max_tokens=10, name="text", temperature=0.5)
+        lm += gen(max_tokens=10, name="text")
         lm += "Pick a number: "
 
+    print(str(lm))
     assert len(lm["text"]) > 0
     assert str(lm).endswith("Pick a number: <|im_end|>")
 
     with user():
-        lm += "I pick 10. Can you pick a number between 0 and 20?"
+        lm += "10. Now you pick a number between 0 and 20"
 
     with assistant():
         lm += gen(max_tokens=2, name="number")
 
     print(str(lm))
-    assert len(lm["number"]) < 0
+    assert len(lm["number"]) > 0
