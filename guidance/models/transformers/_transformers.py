0f8dbec63fcfd80e6cea1819448a10afc523b755
Harsha Nori <harsha.nori@live.com>
2024-05-02 22:02:28 -0700

    black

--- guidance/models/transformers/_transformers.py
+++ guidance/models/transformers/_transformers.py
@@ -262,7 +262,9 @@ class TransformersEngine(Engine):
             self._past_key_values = model_out.past_key_values
             cache_token_ids.extend(new_token_ids)
             # Need to add special truncating logic here for weird models that have a different output size than tokenizer vocab
-            self._cached_logits = model_out.logits[0, -1, :len(self.tokenizer.tokens)].cpu().numpy()
+            self._cached_logits = (
+                model_out.logits[0, -1, : len(self.tokenizer.tokens)].cpu().numpy()
+            )
 
         return self._cached_logits
 
