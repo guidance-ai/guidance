ce0d737895c7f81cc39a1f41f10c6b741d0e9ad2
Harsha Nori <harsha.nori@live.com>
2024-05-17 22:25:59 -0700

    fix capture with named kwarg and add grammar test for it

--- guidance/library/_capture.py
+++ guidance/library/_capture.py
@@ -2,7 +2,7 @@ from .._guidance import guidance
 from .._grammar import capture as grammar_capture, GrammarFunction
 
 
-@guidance(stateless=lambda *args: isinstance(args[0], GrammarFunction))
+@guidance(stateless=lambda *args, **kwargs: isinstance(args[0], GrammarFunction))
 def capture(lm, value, name):
     if isinstance(value, GrammarFunction):
         return lm + grammar_capture(value, name)
