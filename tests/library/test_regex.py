211850ad01d9d078bc772c5cf4914f63188c8ce8
Hudson Cooper <hncooper96@gmail.com>
2024-05-31 13:07:22 -0700

    Make regex tests more stringent with check_match_failure

--- tests/library/test_regex.py
+++ tests/library/test_regex.py
@@ -1,6 +1,9 @@
 import pytest
-from guidance._parser import ParserException
+
 from guidance import regex
+from guidance._grammar import Byte, ByteRange
+
+from ..utils import check_match_failure
 
 
 class TestCharacterClasses:
@@ -25,28 +28,109 @@ class TestCharacterClasses:
         assert regex(pattern).match(string) is not None
 
     @pytest.mark.parametrize(
-        "pattern, string, failure_byte",
+        "pattern, string, good_bytes, failure_byte, allowed_bytes",
         [
-            (r"[abc]", "x", b"x"),
-            (r"[a-z]", "g1", b"1"),
-            (r"[0-9]", "x", b"x"),
-            (r"[b-y]", "a", b"a"),  # range doesn't overflow left
-            (r"[b-y]", "z", b"z"),  # range doesn't overflow right
-            (r"[a-f0-9]", "g", b"g"),
-            (r"[abcA-Z]", "z", b"z"),
-            (r"[a-z\d]", "Z", b"Z"),
-            (r"[^abc]", "b", b"b"),
-            (r"[^\d]", "1", b"1"),
-            (r"[^B-Z]", "B", b"B"),
-            (r"[^a-z\d]", "a", b"a"),
-            (r"[^a-z\d]", "5", b"5"),
-            (r"[^\n]+", "ABCxyz8743-!@#$%^&*()_+\n", b"\n"),
+            (
+                r"[abc]+",
+                "cbabbaccabcx",
+                b"cbabbaccabc",
+                b"x",
+                {Byte(b"a"), Byte(b"b"), Byte(b"c")},
+            ),
+            (
+                r"[a-z]+",
+                "thequickbrownfoxjumpsoverthelazydogX",
+                b"thequickbrownfoxjumpsoverthelazydog",
+                b"X",
+                {ByteRange((b"az"))},
+            ),
+            (
+                r"[0-9]+",
+                "9876543210x",
+                b"9876543210",
+                b"x",
+                {ByteRange((b"09"))},
+            ),
+            (
+                r"[b-y]+",
+                "bya",
+                b"by",
+                b"a",
+                {ByteRange(b"by")},
+            ),  # range doesn't overflow left
+            (
+                r"[b-y]+",
+                "byz",
+                b"by",
+                b"z",
+                {ByteRange(b"by")},
+            ),  # range doesn't overflow right
+            (
+                r"[a-f0-9]+",
+                "abcdef0123456789x",
+                b"abcdef0123456789",
+                b"x",
+                {ByteRange(b"af"), ByteRange(b"09")},
+            ),
+            (
+                r"[abcA-Z]+",
+                "abcABCXYZx",
+                b"abcABCXYZ",
+                b"x",
+                {Byte(b"a"), Byte(b"b"), Byte(b"c"), ByteRange(b"AZ")},
+            ),
+            (
+                r"[a-z\d]+",
+                "abc123@",
+                b"abc123",
+                b"@",
+                {ByteRange(b"az"), ByteRange(b"09")},
+            ),
+            (
+                r"[^abc]+",
+                "ABCxyz8743-!@#$%^&*()_+a",
+                b"ABCxyz8743-!@#$%^&*()_+",
+                b"a",
+                {ByteRange(b"\x00`"), ByteRange(b"d\x7f")},
+            ),
+            (
+                r"[^\d]+",
+                "abcXYZ-!@#$%^&*()_+6",
+                b"abcXYZ-!@#$%^&*()_+",
+                b"6",
+                {ByteRange(b"\x00/"), ByteRange(b":\x7f")},
+            ),
+            (
+                r"[^B-Z]+",
+                "qwertyAB",
+                b"qwertyA",
+                b"B",
+                {ByteRange(b"\x00A"), ByteRange(b"[\x7f")},
+            ),
+            (
+                r"[^a-z\d]+",
+                "ABCDEF-!@#$%^&*()_+x",
+                b"ABCDEF-!@#$%^&*()_+",
+                b"x",
+                {ByteRange(b"\x00/"), ByteRange(b":`"), ByteRange(b"{\x7f")},
+            ),
+            (
+                r"[^\n]+",
+                "ABCxyz8743-!@#$%^&*()_+\n",
+                b"ABCxyz8743-!@#$%^&*()_+",
+                b"\n",
+                {ByteRange(b"\x00\t"), ByteRange(b"\x0b\x7f")},
+            ),
         ],
     )
-    def test_bad(self, pattern, string, failure_byte):
-        with pytest.raises(ParserException) as pe:
-            regex(pattern).match(string, raise_exceptions=True)
-        assert pe.value.current_byte == failure_byte
+    def test_bad(self, pattern, string, good_bytes, failure_byte, allowed_bytes):
+        check_match_failure(
+            bad_string=string,
+            good_bytes=good_bytes,
+            failure_byte=failure_byte,
+            allowed_bytes=allowed_bytes,
+            grammar=regex(pattern),
+        )
 
 
 class TestQuantifiers:
@@ -89,24 +173,97 @@ class TestQuantifiers:
         assert regex(pattern).match(string) is not None
 
     @pytest.mark.parametrize(
-        "pattern, string, failure_byte",
+        "pattern, string, good_bytes, failure_byte, allowed_bytes",
         [
-            (r"a*b", "axb", b"x"),  # 'x' disrupts the match
-            (r"a+b", "b", b"b"),  # 'a+' requires at least one 'a' before 'b'
-            (r"a?b", "axb", b"x"),  # 'x' disrupts the match
-            (r"a?b", "aab", b"a"),  # Second 'a' is too many
-            (r"(xyz)?abc", "xyabc", b"a"),  # Expected 'z'
-            (r"(xyz)?abc", "abcx", b"x"),  # Extra character after 'abc'
-            (r"a{2,4}", "aaaaa", b"a"),  # Too many 'a's
-            (r"(ab){2,3}", "abababab", b"a"),  # 'ab' four times, more than the maximum
-            (r"a{3,5}b", "aab", b"b"),  # Less than the minimum 'a's before 'b'
-            (r"a{3,5}b", "aaaaaab", b"a"),  # More than the maximum 'a's before 'b'
+            (
+                r"a*b",
+                "axb",
+                b"a",
+                b"x",
+                {Byte(b"a"), Byte(b"b")},
+            ),  # 'x' disrupts the match
+            (
+                r"a+b",
+                "b",
+                b"",
+                b"b",
+                {Byte(b"a")},
+            ),  # 'a+' requires at least one 'a' before 'b'
+            (
+                r"a?b",
+                "x",
+                b"",
+                b"x",
+                {Byte(b"a"), Byte(b"b")},
+            ),  # 'a?' requires zero or one 'a' before 'b'
+            (
+                r"a?b",
+                "axb",
+                b"a",
+                b"x",
+                {Byte(b"b")},
+            ),  # 'x' disrupts the match
+            (
+                r"a?b",
+                "aab",
+                b"a",
+                b"a",
+                {Byte(b"b")},
+            ),  # Second 'a' is too many
+            (
+                r"(xyz)?abc",
+                "xyabc",
+                b"xy",
+                b"a",
+                {Byte(b"z")},
+            ),  # Expected 'z'
+            (
+                r"(xyz)?abc",
+                "abcx",
+                b"abc",
+                b"x",
+                set(),
+            ),  # Extra character after 'abc'
+            (
+                r"a{2,4}",
+                "aaaaa",
+                b"aaaa",
+                b"a",
+                set(),
+            ),  # Too many 'a's
+            (
+                r"(ab){2,3}",
+                "abababab",
+                b"ababab",
+                b"a",
+                set(),
+            ),  # 'ab' four times, more than the maximum
+            (
+                r"a{3,5}b",
+                "aab",
+                b"aa",
+                b"b",
+                {Byte(b"a")},
+            ),  # Less than the minimum 'a's before 'b'
+            (
+                r"a{3,5}b",
+                "aaaaaab",
+                b"aaaaa",
+                b"a",
+                {Byte(b"b")},
+            ),  # More than the maximum 'a's before 'b'
         ],
     )
-    def test_quantifiers_failure(self, pattern, string, failure_byte):
-        with pytest.raises(ParserException) as pe:
-            regex(pattern).match(string, raise_exceptions=True)
-        assert pe.value.current_byte == failure_byte
+    def test_quantifiers_failure(
+        self, pattern, string, good_bytes, failure_byte, allowed_bytes
+    ):
+        check_match_failure(
+            bad_string=string,
+            good_bytes=good_bytes,
+            failure_byte=failure_byte,
+            allowed_bytes=allowed_bytes,
+            grammar=regex(pattern),
+        )
 
 
 class TestAlternations:
@@ -159,25 +316,69 @@ class TestAlternations:
         assert regex(pattern).match(string) is not None
 
     @pytest.mark.parametrize(
-        "pattern, string, failure_byte",
+        "pattern, string, good_bytes, failure_byte, allowed_bytes",
         [
-            (r"a|b", "c", b"c"),  # Neither 'a' nor 'b'
-            (r"cat|dog", "car", b"r"),  # Neither 'cat' nor 'dog'
-            (r"apple|orange", "banana", b"b"),  # Neither 'apple' nor 'orange'
-            (r"100|200", "300", b"3"),  # Neither '100' nor '200'
-            (r"(a|b)c|d", "ae", b"e"),  # Neither 'ac' nor 'bc' nor 'd'
-            (r"(a|b)+", "abbaabbabc", b"c"),  # 'c' does not match pattern '(a|b)+'
+            (
+                r"a|b",
+                "c",
+                b"",
+                b"c",
+                {Byte(b"a"), Byte(b"b")},
+            ),  # Neither 'a' nor 'b'
+            (
+                r"apple|orange",
+                "banana",
+                b"",
+                b"b",
+                {Byte(b"a"), Byte(b"o")},
+            ),  # Neither 'apple' nor 'orange'
+            (
+                r"100|200",
+                "300",
+                b"",
+                b"3",
+                {Byte(b"1"), Byte(b"2")},
+            ),  # Neither '100' nor '200'
+            (
+                r"(a|b)c|d",
+                "ae",
+                b"a",
+                b"e",
+                {Byte(b"c"), Byte(b"c")},
+            ),  # Neither 'ac' nor 'bc' nor 'd'
+            (
+                r"(a|b)+",
+                "abbaabbabc",
+                b"abbaabbab",
+                b"c",
+                {Byte(b"a"), Byte(b"b")},
+            ),  # 'c' does not match pattern '(a|b)+'
+            (
+                r"cat|dog",
+                "car",
+                b"ca",
+                b"r",
+                {Byte(b"t")},
+            ),  # 't' should be forced
             (
                 r"(dog|cat)s?",
                 "cars",
+                b"ca",
                 b"r",
-            ),  # 'cars' does not match 'dog' or 'cat' with optional 's'
+                {Byte(b"t")},
+            ),  # 't' should be forced
         ],
     )
-    def test_alternations_failures(self, pattern, string, failure_byte):
-        with pytest.raises(ParserException) as pe:
-            regex(pattern).match(string, raise_exceptions=True)
-        assert pe.value.current_byte == failure_byte
+    def test_alternations_failures(
+        self, pattern, string, good_bytes, failure_byte, allowed_bytes
+    ):
+        check_match_failure(
+            bad_string=string,
+            good_bytes=good_bytes,
+            failure_byte=failure_byte,
+            allowed_bytes=allowed_bytes,
+            grammar=regex(pattern),
+        )
 
 
 class TestDot:
@@ -191,15 +392,27 @@ class TestDot:
         assert regex(pattern).match(string) is not None
 
     @pytest.mark.parametrize(
-        "pattern, string, failure_byte",
+        "pattern, string, good_bytes, failure_byte, allowed_bytes",
         [
-            (r".+", "ABCxyz\n8743-!@#$%^&*()_+", b"\n"),
+            (
+                r".+",
+                "ABCxyz8743-!@#$%^&*()_+\n",
+                b"ABCxyz8743-!@#$%^&*()_+",
+                b"\n",
+                {ByteRange(b"\x00\t"), ByteRange(b"\x0b\x7f")},
+            ),
         ],
     )
-    def test_dot_failures(self, pattern, string, failure_byte):
-        with pytest.raises(ParserException) as pe:
-            regex(pattern).match(string, raise_exceptions=True)
-        assert pe.value.current_byte == failure_byte
+    def test_dot_failures(
+        self, pattern, string, good_bytes, failure_byte, allowed_bytes
+    ):
+        check_match_failure(
+            bad_string=string,
+            good_bytes=good_bytes,
+            failure_byte=failure_byte,
+            allowed_bytes=allowed_bytes,
+            grammar=regex(pattern),
+        )
 
 
 class TestSpecialCharacters:
@@ -223,19 +436,71 @@ class TestSpecialCharacters:
     def test_good(self, pattern, string):
         assert regex(pattern).match(string) is not None
 
-    # TODO: more extensive negative tests
     @pytest.mark.parametrize(
-        "pattern, string, failure_byte",
+        "pattern, string, good_bytes, failure_byte, allowed_bytes",
         [
-            (r"\d", "x", b"x"),
-            (r"\D", "1", b"1"),
-            (r"\w", " ", b" "),
-            (r"\W", "x", b"x"),
-            (r"\s", "x", b"x"),
-            (r"\S", " ", b" "),
+            (
+                r"\d+",
+                "0123456789x",
+                b"0123456789",
+                b"x",
+                {ByteRange(b"09")},
+            ),
+            (
+                r"\D+",
+                "ABCxyz-!@#$%^&*()_+1",
+                b"ABCxyz-!@#$%^&*()_+",
+                b"1",
+                {ByteRange(b"\x00/"), ByteRange(b":\x7f")},
+            ),
+            (
+                r"\w+",
+                "abcABC123_@",
+                b"abcABC123_",
+                b"@",
+                {ByteRange(b"az"), ByteRange(b"AZ"), ByteRange(b"09"), Byte(b"_")},
+            ),
+            (
+                r"\W+",
+                " -!@#$%^&*()+a",
+                b" -!@#$%^&*()+",
+                b"a",
+                {
+                    ByteRange(b"\x00/"),
+                    ByteRange(b":@"),
+                    ByteRange(b"[^"),
+                    Byte(b"`"),
+                    ByteRange(b"{\x7f"),
+                },
+            ),
+            (
+                r"\s+",
+                " \t\n\r\f\v8",
+                b" \t\n\r\f\v",
+                b"8",
+                {
+                    Byte(b" "),
+                    Byte(b"\t"),
+                    Byte(b"\n"),
+                    Byte(b"\r"),
+                    Byte(b"\f"),
+                    Byte(b"\v"),
+                },
+            ),
+            (
+                r"\S+",
+                "abcABC123_ ",
+                b"abcABC123_",
+                b" ",
+                {ByteRange(b"\x00\x08"), ByteRange(b"\x0e\x1f"), ByteRange(b"!\x7f")},
+            ),
         ],
     )
-    def test_bad(self, pattern, string, failure_byte):
-        with pytest.raises(ParserException) as pe:
-            regex(pattern).match(string, raise_exceptions=True)
-        assert pe.value.current_byte == failure_byte
+    def test_bad(self, pattern, string, good_bytes, failure_byte, allowed_bytes):
+        check_match_failure(
+            bad_string=string,
+            good_bytes=good_bytes,
+            failure_byte=failure_byte,
+            allowed_bytes=allowed_bytes,
+            grammar=regex(pattern),
+        )
