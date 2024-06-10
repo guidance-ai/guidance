2ed12e06923524b37832f5aab9243bff0821951d
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2024-06-10 15:35:41 -0400

    Splitting library

---
+++ tests/modelintegration/library/test_commit_point.py
@@ -0,0 +1,14 @@
+from guidance import Tool, capture, commit_point, models, select, string
+
+
+def test_commit_point(selected_model: models.Model):
+    lm = selected_model
+    tools = [Tool(callable=lambda x: x)]
+    stop_pattern = select([string(lm.engine.tokenizer.eos_token)])
+    gen_grammar = select(
+        [commit_point(stop_pattern, hidden=True)]
+        + [
+            capture(commit_point(x.call_grammar, hidden=True), name=f"tool{i}")
+            for i, x in enumerate(tools)
+        ]
+    )
--- tests/unit/library/test_commit_point.py
+++ tests/unit/library/test_commit_point.py
@@ -1,20 +1,7 @@
-from guidance import Tool, capture, commit_point, models, select, string
+from guidance import commit_point, models
 
 
 def test_hidden():
     model = models.Mock()
     model += " one" + commit_point(" two", hidden=True) + " three"
     assert str(model) == " one three"
-
-
-def test_commit_point(selected_model):
-    lm = selected_model
-    tools = [Tool(callable=lambda x: x)]
-    stop_pattern = select([string(lm.engine.tokenizer.eos_token)])
-    gen_grammar = select(
-        [commit_point(stop_pattern, hidden=True)]
-        + [
-            capture(commit_point(x.call_grammar, hidden=True), name=f"tool{i}")
-            for i, x in enumerate(tools)
-        ]
-    )
