34c93587155abe12338810ab01b2b4654dec9af0
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2024-06-13 11:21:47 -0400

    Trying to expand a little on tests

--- tests/test_tokenizers.py
+++ tests/test_tokenizers.py
@@ -10,8 +10,28 @@ class TestTransformerTokenizers:
             "gpt2",
             "microsoft/Phi-3-mini-4k-instruct",
             "microsoft/Phi-3-vision-128k-instruct",
+            "microsoft/phi-2",
         ],
     )
     def test_smoke(self, model_name: str):
         my_tok = models.TransformersTokenizer(model=model_name, tokenizer=None)
         assert my_tok is not None
+
+    @pytest.mark.parametrize(
+        "model_name",
+        [
+            "gpt2",
+            "microsoft/Phi-3-mini-4k-instruct",
+            "microsoft/Phi-3-vision-128k-instruct",
+            "microsoft/phi-2",
+        ],
+    )
+    @pytest.mark.parametrize("target_string", ["hello", "’"])
+    def test_string_roundtrip(self, model_name: str, target_string: str):
+        my_tok = models.TransformersTokenizer(model=model_name, tokenizer=None)
+
+        encoded = my_tok.encode(target_string.encode())
+        decoded = my_tok.decode(encoded)
+        final_string = decoded.decode()
+
+        assert final_string == target_string
