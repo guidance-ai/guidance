3577e0bf8cd9aa71ff25b1f309ae3a3510fa8f2e
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2024-06-14 09:24:39 -0400

    Refactor a little

--- tests/test_tokenizers.py
+++ tests/test_tokenizers.py
@@ -4,28 +4,22 @@ from guidance import models
 
 
 class TestTransformerTokenizers:
+    TRANSFORMER_MODELS = [
+        "gpt2",
+        "microsoft/Phi-3-mini-4k-instruct",
+        "microsoft/Phi-3-vision-128k-instruct",
+        "microsoft/phi-2",
+    ]
+
     @pytest.mark.parametrize(
         "model_name",
-        [
-            "gpt2",
-            "microsoft/Phi-3-mini-4k-instruct",
-            "microsoft/Phi-3-vision-128k-instruct",
-            "microsoft/phi-2",
-        ],
+        TRANSFORMER_MODELS,
     )
     def test_smoke(self, model_name: str):
         my_tok = models.TransformersTokenizer(model=model_name, tokenizer=None)
         assert my_tok is not None
 
-    @pytest.mark.parametrize(
-        "model_name",
-        [
-            "gpt2",
-            "microsoft/Phi-3-mini-4k-instruct",
-            "microsoft/Phi-3-vision-128k-instruct",
-            "microsoft/phi-2",
-        ],
-    )
+    @pytest.mark.parametrize("model_name", TRANSFORMER_MODELS)
     @pytest.mark.parametrize("target_string", ["hello", "’"])
     def test_string_roundtrip(self, model_name: str, target_string: str):
         my_tok = models.TransformersTokenizer(model=model_name, tokenizer=None)
