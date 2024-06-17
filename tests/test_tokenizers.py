7f83946112aaa32e59d352484ce6223e97a65403
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2024-06-17 14:57:38 -0400

    Don't need LlamaCpp tokeniser tests any more

--- tests/test_tokenizers.py
+++ tests/test_tokenizers.py
@@ -50,46 +50,6 @@ class TestTransformerTokenizers:
         assert final_string == target_string
 
 
-class TestLlamaCppTokenizers:
-    LLAMACPP_MODELS = [
-        dict(
-            gguf="TheBloke/Llama-2-7B-GGUF:llama-2-7b.Q5_K_M.gguf",
-            kwargs={"verbose": True, "n_ctx": 4096},
-        ),
-        dict(
-            gguf="microsoft/Phi-3-mini-4k-instruct-gguf:Phi-3-mini-4k-instruct-q4.gguf",
-            kwargs={"verbose": True, "n_ctx": 4096},
-        ),
-        dict(
-            gguf="TheBloke/Mistral-7B-Instruct-v0.2-GGUF:mistral-7b-instruct-v0.2.Q8_0.gguf",
-            kwargs={"verbose": True},
-        ),
-    ]
-
-    def get_tokenizer(self, model_info: dict[str, Any]):
-        repo_id, gguf_file = model_info["gguf"].split(":")
-        downloaded_file = hf_hub_download(repo_id=repo_id, filename=gguf_file)
-        lm = models.LlamaCpp(model=downloaded_file, **model_info["kwargs"])
-        my_tok = lm.engine.tokenizer
-        return my_tok
-
-    @pytest.mark.parametrize("model_info", LLAMACPP_MODELS)
-    def test_smoke(self, model_info: dict[str, Any]):
-        my_tok = self.get_tokenizer(model_info)
-        assert my_tok is not None
-
-    @pytest.mark.parametrize("model_info", LLAMACPP_MODELS)
-    @pytest.mark.parametrize("target_string", ROUND_TRIP_STRINGS)
-    def test_string_roundtrip(self, model_info: dict[str, Any], target_string: str):
-        my_tok = self.get_tokenizer(model_info)
-
-        encoded = my_tok.encode(target_string.encode())
-        decoded = my_tok.decode(encoded)
-        final_string = decoded.decode()
-
-        assert final_string == target_string
-
-
 class TestTiktoken:
     MODELS = ["gpt-3.5-turbo", "gpt-4"]
 
