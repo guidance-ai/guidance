815ed6bceb9453288dbdd1d689a66259320eade1
Hudson Cooper <hncooper96@gmail.com>
2024-06-26 18:26:15 -0700

    Using commit point as a regex (ish)

--- tests/unit/library/test_commit_point.py
+++ tests/unit/library/test_commit_point.py
@@ -1,7 +1,17 @@
-from guidance import commit_point, models
+from guidance import commit_point, one_or_more, byte_range, models
 
 
 def test_hidden():
     model = models.Mock()
     model += " one" + commit_point(" two", hidden=True) + " three"
     assert str(model) == " one three"
+
+
+def test_pseudo_regex():
+    grm = commit_point(
+        '"' + byte_range(b"A", b"Z") + one_or_more(byte_range(b"a", b"z")) + '"'
+    )
+    assert grm.match('"Abcd"') is not None
+    assert grm.match('"Zyxwv"') is not None
+    assert grm.match('"A"') is None
+    assert grm.match('"abc"') is None
