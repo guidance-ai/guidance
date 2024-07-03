733da2c7d337b49486c23538d6eeaabea2439174
Hudson Cooper <hncooper96@gmail.com>
2024-07-03 16:37:28 -0700

    Add explicit xfails for commit points

--- tests/unit/library/test_commit_point.py
+++ tests/unit/library/test_commit_point.py
@@ -1,6 +1,7 @@
+import pytest
 from guidance import commit_point, one_or_more, byte_range, models
 
-
+@pytest.mark.xfail(reason="Hidden commit points are not supported")
 def test_hidden():
     model = models.Mock()
     model += " one" + commit_point(" two", hidden=True) + " three"
