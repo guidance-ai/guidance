b60641b590f2b12561cd5a9081069f39b8e5df44
Hudson Cooper <hncooper96@gmail.com>
2024-07-03 16:28:31 -0700

    Make test less flaky

--- tests/model_integration/library/test_gen.py
+++ tests/model_integration/library/test_gen.py
@@ -156,8 +156,8 @@ def test_non_token_force(selected_model: models.Model):
     "pattern",
     [
         "(Scott is a person|Scott is a persimmon)",
-        r"Scott is a persimmon.*\.",
-        r"\d\.*\d+",
+        r"Scott is a persimmon.{0,20}\.",
+        r"\d\.{0,20}\d+",
     ],
 )
 def test_various_regexes(selected_model: models.Model, prompt: str, pattern: str):
