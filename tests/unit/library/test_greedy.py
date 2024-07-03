b00a69534b85a9b4e89e213149f560b3a6b95dac
Hudson Cooper <hncooper96@gmail.com>
2024-07-02 17:28:35 -0700

    greedy tests for ambiguous ending lexemes

---
+++ tests/unit/library/test_greedy.py
@@ -0,0 +1,27 @@
+import pytest
+from guidance import greedy_grammar, lexeme
+
+class TestEndingLexemeAmbiguous:
+    @pytest.mark.parametrize(
+        "string",
+        ["123"]
+    )
+    def test_no_skip_rx(self, string):
+        g1 = greedy_grammar(body=lexeme(r"\d+"), name="mycap")
+        assert (m := g1.match(string)) is not None and m.captures["mycap"] == string
+        g2 = g1 + "x"
+        assert (m := g2.match(f"{string}x")) is not None and m.captures["mycap"] == string
+
+    @pytest.mark.parametrize(
+        "whitespace_rx",
+        [r"\s", r"\s+", r"\s*"]
+    )
+    @pytest.mark.parametrize(
+        "string",
+        ["123", "123 ", "123  "]
+    )
+    def test_skip_rx(self, string, whitespace_rx):
+        g1 = greedy_grammar(body=lexeme(r"\d+"), skip_regex=whitespace_rx, name="mycap")
+        assert (m := g1.match(string)) is not None and m.captures["mycap"] == string
+        g2 = g1 + "x"
+        assert (m := g2.match(f"{string}x")) is not None and m.captures["mycap"] == string
