3b2e762173edd80cc8f364d4caa2591329fb1a40
Michal Moskal <michal@moskal.me>
2024-07-10 03:31:08 +0000

    mark failing tool test as xfail

--- tests/model_integration/library/test_gen.py
+++ tests/model_integration/library/test_gen.py
@@ -224,6 +224,7 @@ Step 1"""
     assert True
 
 
+@pytest.mark.xfail
 def test_tool_call(selected_model: models.Model):
     import guidance
     from guidance import Tool, capture, one_or_more, select, zero_or_more
