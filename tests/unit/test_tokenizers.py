16e4a7085e0f4f56ba2dc63158d542be6b2e87cf
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2024-07-18 19:00:04 +0000

    Expand tokeniser testing

--- tests/unit/test_tokenizers.py
+++ tests/unit/test_tokenizers.py
@@ -19,8 +19,10 @@ from tests.constants import TOKENIZER_ROUND_TRIP_STRINGS
 class TestTransformerTokenizers:
     TRANSFORMER_MODELS = [
         "gpt2",
-        "microsoft/Phi-3-mini-4k-instruct",
         "microsoft/phi-2",
+        "microsoft/Phi-3-small-8k-instruct",
+        "microsoft/Phi-3-mini-4k-instruct",
+        "meta-llama/Meta-Llama-3-8B-Instruct",
     ]
 
     @pytest.mark.parametrize(
@@ -28,13 +30,17 @@ class TestTransformerTokenizers:
         TRANSFORMER_MODELS,
     )
     def test_smoke(self, model_name: str):
-        my_tok = models.TransformersTokenizer(model=model_name, transformers_tokenizer=None)
+        my_tok = models.TransformersTokenizer(
+            model=model_name, transformers_tokenizer=None, trust_remote_code=True
+        )
         assert my_tok is not None
 
     @pytest.mark.parametrize("model_name", TRANSFORMER_MODELS)
     @pytest.mark.parametrize("target_string", TOKENIZER_ROUND_TRIP_STRINGS)
     def test_string_roundtrip(self, model_name: str, target_string: str):
-        my_tok = models.TransformersTokenizer(model=model_name, transformers_tokenizer=None)
+        my_tok = models.TransformersTokenizer(
+            model=model_name, transformers_tokenizer=None, trust_remote_code=True
+        )
 
         encoded = my_tok.encode(target_string.encode())
         decoded = my_tok.decode(encoded)
