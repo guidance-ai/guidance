6ac6b9b3831d7a375061a3623a2dd9116b99b00b
Harsha Nori <harsha.nori@live.com>
2024-07-20 21:52:42 -0700

    Initial dedent fix & simple test

--- tests/unit/test_model.py
+++ tests/unit/test_model.py
@@ -1,6 +1,8 @@
 import guidance
-
 from guidance import gen, models
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
 def test_call_embeddings():
     """This tests calls embedded in strings."""
@@ -21,3 +23,35 @@ def test_call_embeddings():
         return lm
 
     assert "{{G|" not in str(model + ble())
+
+# def test_dedent():
+#     """Test that dedent functionality in f-strings works across Python versions."""
+#     @guidance(stateless=True, dedent=True)
+#     def character_maker(lm):
+#         lm += f"""\
+#         {{
+#             "name": "{1+1}",
+#         }}"""
+#         return lm
+
+#     lm = guidance.models.Mock()
+#     lm += character_maker()
+#     assert str(lm).startswith("{")
+
+
+
+def test_dedent():
+    """Test that dedent functionality in f-strings works across Python versions."""
+    @guidance(stateless=True, dedent=True)
+    def character_maker(lm):
+        lm += f"""\
+        {{
+            "name": "{1+1}",
+            "age": "{gen('name', stop='"', max_tokens=1)}",
+        }}"""
+        return lm
+
+    lm = guidance.models.Mock()
+    result = lm + character_maker()
+    # logging.debug(f"Result: {str(result)}")
+    assert str(result).startswith("{")
