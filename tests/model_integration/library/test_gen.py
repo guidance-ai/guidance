aad4b5ae431e7c2464ab6f791f62c488843805f9
Hudson Cooper <hncooper96@gmail.com>
2024-07-22 11:03:32 -0700

    \d -> [0-9] to prevent max_tokens from cutting off non-unicode digits

--- tests/model_integration/library/test_gen.py
+++ tests/model_integration/library/test_gen.py
@@ -157,7 +157,7 @@ def test_non_token_force(selected_model: models.Model):
     [
         "(Scott is a person|Scott is a persimmon)",
         r"Scott is a persimmon.{0,20}\.",
-        r"\d\.{0,20}\d+",
+        r"[0-9]\.{0,20}[0-9]+",
     ],
 )
 def test_various_regexes(selected_model: models.Model, prompt: str, pattern: str):
