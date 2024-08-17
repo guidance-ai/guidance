ffffa5c3e2d7da5cfd849b6a106e301718ed570e
Hudson Cooper <hncooper96@gmail.com>
2024-08-16 18:10:30 -0700

    Prompt to encourage shorter output

--- tests/model_integration/library/test_subgrammar.py
+++ tests/model_integration/library/test_subgrammar.py
@@ -68,7 +68,7 @@ def gen_json_object(lm, name: str, max_tokens=100000000):
 
 def test_greedy_json_object(selected_model: guidance.models.Model):
     lm = selected_model
-    lm += "Very short info about John Doe:\n"
+    lm += "JSON of John Doe's name, age, and birthday:\n"
     lm += gen_json_object("hacker", max_tokens=1000)
     lm += "\nScore: " + gen("score", regex="[1-3]")
     # make sure it parses as JSON
