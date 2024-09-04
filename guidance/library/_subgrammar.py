ceefc2c02b1921518dd4347b0ccd9e4556c1f03d
Hudson Cooper <hncooper96@gmail.com>
2024-09-04 10:31:42 -0700

    Add 'json_string' to lexeme

--- guidance/library/_subgrammar.py
+++ guidance/library/_subgrammar.py
@@ -5,8 +5,22 @@ from typing import Optional
 def lexeme(
     body_regex: str,
     contextual: bool = False,
-):
-    return Lexeme(body_regex=body_regex, contextual=contextual)
+    json_string: bool = False,
+) -> Lexeme:
+    """
+    Constructs a Lexeme based on a given regular expression.
+
+    Parameters:
+    body_regex (str): The regular expression that will greedily match the input.
+    contextual (bool): If false, all other lexemes are excluded when this lexeme is recognized.
+        This is normal behavior for keywords in programming languages.
+        Set to true for eg. a JSON schema with both `/"type"/` and `/"[^"]*"/` as lexemes,
+        or for "get"/"set" contextual keywords in C#.
+    json_string (bool): Specifies if the lexeme should be quoted as a JSON string.
+        For example, /[a-z"]+/ will be quoted as /([a-z]|\\")+.
+        Defaults to False.
+    """
+    return Lexeme(body_regex=body_regex, contextual=contextual, json_string=json_string)
 
 
 def subgrammar(
