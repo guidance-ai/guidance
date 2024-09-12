3812dc98640a19a6ba5dadccdc161610149a575a
Hudson Cooper <hncooper96@gmail.com>
2024-09-12 10:58:13 -0700

    case insensitive T/Z

--- guidance/library/_json.py
+++ guidance/library/_json.py
@@ -106,7 +106,7 @@ VALID_KEYS = set(Keyword) | IGNORED_KEYS | DEFS_KEYS | set(StringKeywords) | set
 FORMAT_PATTERNS: dict[str, Optional[str]] = {
     # https://json-schema.org/understanding-json-schema/reference/string#built-in-formats
     # Dates and times
-    "date-time": r'[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}(?:\.[0-9]+)?(?:Z|[+-][0-9]{2}:[0-9]{2})',
+    "date-time": r'[0-9]{4}-[0-9]{2}-[0-9]{2}[tT][0-9]{2}:[0-9]{2}:[0-9]{2}(?:\.[0-9]+)?(?:[zZ]|[+-][0-9]{2}:[0-9]{2})',
     "time": r'[0-9]{2}:[0-9]{2}:[0-9]{2}(?:\.[0-9]+)?(?:Z|[+-][0-9]{2}:[0-9]{2})?',
     "date": r'[0-9]{4}-[0-9]{2}-[0-9]{2}',
     "duration": r'P([0-9]+Y)?([0-9]+M)?([0-9]+D)?(T([0-9]+H)?([0-9]+M)?([0-9]+S)?)?',
