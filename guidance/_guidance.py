943a80142d84b0c809771ba877e251c2a7717bcf
Hudson Cooper <hncooper96@gmail.com>
2024-09-20 21:13:25 -0700

    handle partial one level up

--- guidance/_guidance.py
+++ guidance/_guidance.py
@@ -15,6 +15,13 @@ def guidance(
     model = Model,
 ):
     """Decorator used to define guidance grammars"""
+    # if we are not yet being used as a decorator, then save the args
+
+    if f is None:
+        return functools.partial(
+            _decorator, stateless=stateless, cache=cache, dedent=dedent, model=model
+        )
+
     return _decorator(f, stateless=stateless, cache=cache, dedent=dedent, model=model)
 
 def guidance_method(
@@ -74,74 +81,64 @@ _null_grammar = string("")
 
 
 def _decorator(f, *, stateless, cache, dedent, model):
+    # this strips out indentation in multiline strings that aligns with the current python indentation
+    if dedent is True or dedent == "python":
+        f = strip_multiline_string_indents(f)
 
-    # if we are not yet being used as a decorator, then save the args
-    if f is None:
-        return functools.partial(
-            _decorator, stateless=stateless, cache=cache, dedent=dedent, model=model
-        )
+    # we cache if requested
+    if cache:
+        f = functools.cache(f)
 
-    # if we are being used as a decorator then return the decorated function
-    else:
+    @functools.wraps(f)
+    def wrapped(*args, **kwargs):
 
-        # this strips out indentation in multiline strings that aligns with the current python indentation
-        if dedent is True or dedent == "python":
-            f = strip_multiline_string_indents(f)
+        # make a stateless grammar if we can
+        if stateless is True or (
+            callable(stateless) and stateless(*args, **kwargs)
+        ):
 
-        # we cache if requested
-        if cache:
-            f = functools.cache(f)
+            # if we have a (deferred) reference set, then we must be in a recursive definition and so we return the reference
+            reference = getattr(f, "_self_call_reference_", None)
+            if reference is not None:
+                return reference
 
-        @functools.wraps(f)
-        def wrapped(*args, **kwargs):
-
-            # make a stateless grammar if we can
-            if stateless is True or (
-                callable(stateless) and stateless(*args, **kwargs)
-            ):
-
-                # if we have a (deferred) reference set, then we must be in a recursive definition and so we return the reference
-                reference = getattr(f, "_self_call_reference_", None)
-                if reference is not None:
-                    return reference
-
-                # otherwise we call the function to generate the grammar
-                else:
-
-                    # set a DeferredReference for recursive calls (only if we don't have arguments that might make caching a bad idea)
-                    no_args = len(args) + len(kwargs) == 0
-                    if no_args:
-                        f._self_call_reference_ = DeferredReference()
-
-                    try:
-                        # call the function to get the grammar node
-                        node = f(_null_grammar, *args, **kwargs)
-                    except:
-                        raise
-                    else:
-                        if not isinstance(node, (Terminal, str)):
-                            node.name = f.__name__
-                        # set the reference value with our generated node
-                        if no_args:
-                            f._self_call_reference_.value = node
-                    finally:
-                        if no_args:
-                            del f._self_call_reference_
-
-                    return node
-
-            # otherwise must be stateful (which means we can't be inside a select() call)
+            # otherwise we call the function to generate the grammar
             else:
-                return RawFunction(f, args, kwargs)
 
-        # Remove the first argument from the wrapped function
-        signature = inspect.signature(f)
-        params = list(signature.parameters.values())
-        params.pop(0)
-        wrapped.__signature__ = signature.replace(parameters=params)
+                # set a DeferredReference for recursive calls (only if we don't have arguments that might make caching a bad idea)
+                no_args = len(args) + len(kwargs) == 0
+                if no_args:
+                    f._self_call_reference_ = DeferredReference()
 
-        # attach this as a method of the model class (if given)
-        # if model is not None:
-        #     setattr(model, f.__name__, f)
+                try:
+                    # call the function to get the grammar node
+                    node = f(_null_grammar, *args, **kwargs)
+                except:
+                    raise
+                else:
+                    if not isinstance(node, (Terminal, str)):
+                        node.name = f.__name__
+                    # set the reference value with our generated node
+                    if no_args:
+                        f._self_call_reference_.value = node
+                finally:
+                    if no_args:
+                        del f._self_call_reference_
 
-        return wrapped
+                return node
+
+        # otherwise must be stateful (which means we can't be inside a select() call)
+        else:
+            return RawFunction(f, args, kwargs)
+
+    # Remove the first argument from the wrapped function
+    signature = inspect.signature(f)
+    params = list(signature.parameters.values())
+    params.pop(0)
+    wrapped.__signature__ = signature.replace(parameters=params)
+
+    # attach this as a method of the model class (if given)
+    # if model is not None:
+    #     setattr(model, f.__name__, f)
+
+    return wrapped
