a80d95ac50b9c6966c5ec878b3f36a573e1bfdf5
S Jenkins <nopdive@gmail.com>
2024-10-08 22:13:28 -0700

    Minor style change on token grid items.

--- client/graphpaper-inline/src/TokenGridItem.svelte
+++ client/graphpaper-inline/src/TokenGridItem.svelte
@@ -18,20 +18,20 @@
 
 {#each token.value as ch, i}
     {#if ch === ' '}
-        <span on:mouseenter={handleMouseEnter} on:mouseleave={handleMouseLeave} role="tooltip" class={`inline-block mt-2 border-b-2 ${hovered ? "bg-gray-300 text-gray-700" : "text-gray-300"}`} style={`border-bottom-color: ${color(token.prob)}`}>
+        <span on:mouseenter={handleMouseEnter} on:mouseleave={handleMouseLeave} role="tooltip" class={`inline-block mt-2 border-b-2 ${hovered ? "bg-gray-400 text-gray-50" : "text-gray-300"}`} style={`border-bottom-color: ${color(token.prob)}`}>
             &nbsp;
         </span>
     {:else if ch === '\t'}
-        <span on:mouseenter={handleMouseEnter} on:mouseleave={handleMouseLeave} role="tooltip" class={`inline-block mt-2 border-b-2 ${hovered ? "bg-gray-300 text-gray-700" : "text-gray-300"}`} style={`border-bottom-color: ${color(token.prob)}`}>
+        <span on:mouseenter={handleMouseEnter} on:mouseleave={handleMouseLeave} role="tooltip" class={`inline-block mt-2 border-b-2 ${hovered ? "bg-gray-400 text-gray-50" : "text-gray-300"}`} style={`border-bottom-color: ${color(token.prob)}`}>
             \t&nbsp;&nbsp;
         </span>
     {:else if ch === '\n'}
-        <span on:mouseenter={handleMouseEnter} on:mouseleave={handleMouseLeave} role="tooltip" class={`inline-block mt-2 border-b-2 ${hovered ? "bg-gray-300 text-gray-700" : "text-gray-300"}`} style={`border-bottom-color: ${color(token.prob)}`}>
+        <span on:mouseenter={handleMouseEnter} on:mouseleave={handleMouseLeave} role="tooltip" class={`inline-block mt-2 border-b-2 ${hovered ? "bg-gray-400 text-gray-50" : "text-gray-300"}`} style={`border-bottom-color: ${color(token.prob)}`}>
             \n
         </span>
         <div class="basis-full h-full"></div>
     {:else}
-        <span on:mouseenter={handleMouseEnter} on:mouseleave={handleMouseLeave} role="tooltip" class={`inline-block mt-2 border-b-2 ${hovered ? "bg-gray-300 text-gray-700" : token.special ? "text-gray-300" : ""}`} style={`border-bottom-color: ${color(token.prob)}`}>
+        <span on:mouseenter={handleMouseEnter} on:mouseleave={handleMouseLeave} role="tooltip" class={`inline-block mt-2 border-b-2 ${hovered ? "bg-gray-400 text-gray-50" : token.special ? "text-gray-300" : ""}`} style={`border-bottom-color: ${color(token.prob)}`}>
             {#if i === 0}
                 <span class="absolute text-xs uppercase -mt-4 text-purple-800 font-sans">
                     {token.role}
