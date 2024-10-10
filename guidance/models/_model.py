e3f047715e8887a9a478535f9a7901c6cafd242f
Loc Huynh <jc1da.3011@gmail.com>
2024-10-09 17:37:44 -0700

    Add metric worker

--- guidance/models/_model.py
+++ guidance/models/_model.py
@@ -6,7 +6,10 @@ import threading
 
 import time
 from typing import Iterator, Optional, TYPE_CHECKING
-
+from multiprocessing import Manager, Process
+from typing import Any, Union
+from enum import Enum
+import psutil
 
 import numpy as np
 
@@ -1384,3 +1387,163 @@ class ConstraintException(Exception):
         self.prompt = kwargs.pop("prompt", None)
         self.data = kwargs.pop("data", None)
         super().__init__(*args, **kwargs)
+
+class MonitoringMetric(str, Enum):
+    CPU_USAGE = "cpu_usage"
+    MEM_USAGE = "mem_usage"
+    GPU_USAGE = "gpu_usage"
+    GPU_USED_MEM = "gpu_used_mem"
+    GPU_TOTAL_MEM = "gpu_total_mem"
+    INPUT_TOKENS = "input_tokens"
+    OUTPUT_TOKENS = "output_tokens"
+    BACKTRACK_TOKENS = "backtrack_tokens"
+    TOKEN_COUNT = "token_count"
+    TOKEN_REDUCTION = "token_reduction"
+
+ALL_METRICS = [
+    MonitoringMetric.CPU_USAGE,
+    MonitoringMetric.MEM_USAGE,
+    MonitoringMetric.GPU_USAGE,
+    MonitoringMetric.GPU_USED_MEM,
+    MonitoringMetric.GPU_TOTAL_MEM,
+    MonitoringMetric.INPUT_TOKENS,
+    MonitoringMetric.OUTPUT_TOKENS,
+    MonitoringMetric.BACKTRACK_TOKENS,
+    MonitoringMetric.TOKEN_COUNT,
+    MonitoringMetric.TOKEN_REDUCTION
+]
+
+def _monitor_fn(stop_flag, metrics_dict: dict[MonitoringMetric, list], max_size: int = 100, interval_ms: float = 1000):
+    print("Monitoring started")
+
+    to_collect_gpu_stats = False
+    try:
+        import gpustat
+        gpu_stats = gpustat.GPUStatCollection.new_query()
+        if len(gpu_stats) > 0:
+            # only collect GPU stats if there is at least one GPU
+            to_collect_gpu_stats = True
+    except:
+        pass
+
+    try:
+        while not stop_flag.value:
+            t0 = time.time()
+
+            #cpu_percent = psutil.cpu_percent(interval=1)
+            cpu_percent = psutil.cpu_percent()
+            memory_usage = psutil.virtual_memory()
+
+            metrics_dict[MonitoringMetric.CPU_USAGE].append(cpu_percent)
+            metrics_dict[MonitoringMetric.MEM_USAGE].append(memory_usage.percent)
+
+            t1 = time.time()
+
+            if to_collect_gpu_stats:
+                gpu_stats = gpustat.GPUStatCollection.new_query()
+
+                usage = [gpu.utilization for gpu in gpu_stats.gpus]
+                mem_usage = [gpu.memory_used for gpu in gpu_stats.gpus]
+                mem_total = [gpu.memory_total for gpu in gpu_stats.gpus]
+
+                metrics_dict[MonitoringMetric.GPU_USAGE].append(usage)
+                metrics_dict[MonitoringMetric.GPU_USED_MEM].append(mem_usage)
+                metrics_dict[MonitoringMetric.GPU_TOTAL_MEM].append(mem_total)
+
+            t2 = time.time()
+            
+            for metrics in metrics_dict.values():
+                if len(metrics) > max_size:
+                    metrics.pop(0)
+
+            lat = time.time() - t0
+            cpu_lat = t1 - t0
+            gpu_lat = t2 - t1
+
+            # print(f"Monitoring took {lat*1000:.1f}ms")
+            # print(f"CPU/MEM: {cpu_lat*1000:.1f}ms, GPU: {gpu_lat*1000:.1f}ms")
+
+            # sleep for the remaining time of the interval
+            sleep_time = interval_ms / 1000.0 - lat
+            if sleep_time < 0:
+                time.sleep(sleep_time)
+    except Exception as e:
+        # print(f"Error in monitoring: {e}")
+        pass
+
+    print("Monitoring stopped")
+
+class Monitor:
+    """Monitoring service to collect neccessary metrics for visualizatoin
+    """
+    
+    def __init__(self, engine: Engine, **kwargs):
+        self.engine = engine
+        self.mp_manager = Manager()
+
+        # use list instead of queue for easily accessing each item, e.g., last item
+        self.max_size = kwargs.get("max_size", 100)
+        
+        self.metrics_dict = {
+            MonitoringMetric.CPU_USAGE: self.mp_manager.list(),
+            MonitoringMetric.MEM_USAGE: self.mp_manager.list(),
+            MonitoringMetric.GPU_USAGE: self.mp_manager.list(),
+            MonitoringMetric.GPU_USED_MEM: self.mp_manager.list(),
+            MonitoringMetric.GPU_TOTAL_MEM: self.mp_manager.list()
+        }
+
+        self.stop_flag = self.mp_manager.Value("b", False)
+        self.process = None
+
+        self.per_token_metrics = [] # store metrics per token in token list
+
+    def start(self):
+        self.process = Process(target=_monitor_fn, args=(self.stop_flag, self.metrics_dict, self.max_size))
+        self.process.start()
+
+    def stop(self):
+        if self.process:
+            self.stop_flag.value = True
+            self.process.join()
+
+            for metrics in self.metrics_dict.values():
+                metrics.clear()
+
+    def reset(self):
+        self.stop()
+
+        for metrics in self.metrics_dict.values():
+            metrics.clear()
+
+        self.start()
+
+    def get_metrics(self, metrics: list[MonitoringMetric] = ALL_METRICS, lm: Union[Model, None] = None) -> dict[MonitoringMetric, Any]:
+        result = {}
+
+        for metric in metrics:
+            if metric in [
+                MonitoringMetric.CPU_USAGE,
+                MonitoringMetric.MEM_USAGE,
+                MonitoringMetric.GPU_USAGE,
+                MonitoringMetric.GPU_USED_MEM,
+                MonitoringMetric.GPU_TOTAL_MEM
+            ]:
+                result[metric] = self.metrics_dict[metric][-1] if len(self.metrics_dict[metric]) > 0 else None
+            elif metric == MonitoringMetric.INPUT_TOKENS:
+                result[metric] = self.engine.metrics.engine_input_tokens
+            elif metric == MonitoringMetric.OUTPUT_TOKENS:
+                result[metric] = self.engine.metrics.engine_output_tokens
+            elif metric == MonitoringMetric.BACKTRACK_TOKENS:
+                result[metric] = self.engine.metrics.engine_backtrack_tokens
+            elif metric == MonitoringMetric.TOKEN_COUNT:
+                result[metric] = lm.token_count if lm is not None else None
+            elif metric == MonitoringMetric.TOKEN_REDUCTION:
+                if lm is not None and lm.token_count > 0:
+                    result[metric] = 1 - (self.engine.metrics.engine_output_tokens / lm.token_count)
+                else:
+                    result[metric] = None
+        
+        return result
+    
+    def get_metric(self, metric: MonitoringMetric, lm: Union[Model, None] = None) -> Any:
+        return self.get_metrics([metric], lm)[metric]
