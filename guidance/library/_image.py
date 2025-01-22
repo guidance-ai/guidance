315336845bac2c51ff2368d7cfd67c8fd016a70a
S Jenkins <nopdive@gmail.com>
2025-01-22 12:12:19 -0800

    Refactor of byte parsing for multi-modal primitives.
    Primitives were duplicating code.

--- guidance/library/_image.py
+++ guidance/library/_image.py
@@ -1,34 +1,13 @@
-import http
 import pathlib
-import re
 import typing
-import urllib
 
 from .._guidance import guidance
+from .._utils import bytes_from
 
 
 @guidance
 def image(lm, src: typing.Union[str, pathlib.Path, bytes], allow_local: bool = True):
-
-    # load the image bytes
-    # ...from a url
-    if isinstance(src, str) and re.match(r"[^:/]+://", src):
-        with urllib.request.urlopen(src) as response:
-            response = typing.cast(http.client.HTTPResponse, response)
-            bytes_data = response.read()
-
-    # ...from a local path
-    elif allow_local and (isinstance(src, str) or isinstance(src, pathlib.Path)):
-        with open(src, "rb") as f:
-            bytes_data = f.read()
-
-    # ...from image file bytes
-    elif isinstance(src, bytes):
-        bytes_data = src
-
-    else:
-        raise Exception(f"Unable to load image bytes from {src}!")
-
+    bytes_data = bytes_from(src, allow_local=allow_local)
     bytes_id = str(id(bytes_data))
 
     # set the image bytes
--- guidance/library/_video.py
+++ guidance/library/_video.py
@@ -1,35 +1,14 @@
-import http
 import pathlib
-import re
 import typing
-import urllib
 
 from .._guidance import guidance
+from .._utils import bytes_from
 
 
 @guidance
 def video(lm, src: typing.Union[str, pathlib.Path, bytes], allow_local: bool = True):
     # TODO(nopdive): Stub for testing -- needs implementation.
-    # NOTE(nopdive): src parsing is the same per modal, consider refactoring.
-
-    # load the video bytes
-    # ...from a url
-    if isinstance(src, str) and re.match(r"[^:/]+://", src):
-        with urllib.request.urlopen(src) as response:
-            response = typing.cast(http.client.HTTPResponse, response)
-            bytes_data = response.read()
-
-    # ...from a local path
-    elif allow_local and (isinstance(src, str) or isinstance(src, pathlib.Path)):
-        with open(src, "rb") as f:
-            bytes_data = f.read()
-
-    # ...from video file bytes
-    elif isinstance(src, bytes):
-        bytes_data = src
-
-    else:
-        raise Exception(f"Unable to load video bytes from {src}!")
+    bytes_data = bytes_from(src, allow_local=allow_local)
 
     # Add video to LM.
     # lm += ...
