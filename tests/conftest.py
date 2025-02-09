5dda5d90485956cd49f70063839d4881b21bc304
Paul Koch <code@koch.ninja>
2025-02-08 20:33:20 -0800

    create the model objects directly in the AVAILABLE_MODELS dictionary

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,115 +18,121 @@ from guidance import models
 AVAILABLE_MODELS = {}
 
 # GEMMA 2
-AVAILABLE_MODELS["llamacpp_gemma2_9b_cpu"] = lambda: dict(
-    # Note that this model requires an appropriate
-    # HF_TOKEN environment variable
-    name="huggingface_hubllama:bartowski/gemma-2-9b-it-GGUF:gemma-2-9b-it-IQ2_XS.gguf",
-    kwargs={"verbose": True, "n_ctx": 4096},
+# Note that this model requires an appropriate HF_TOKEN environment variable
+AVAILABLE_MODELS["llamacpp_gemma2_9b_cpu"] = lambda: models.LlamaCpp(
+    importlib.import_module("huggingface_hub").hf_hub_download(
+        repo_id="bartowski/gemma-2-9b-it-GGUF", filename="gemma-2-9b-it-IQ2_XS.gguf"
+    ),
+    verbose=True,
+    n_ctx=4096,
 )
-AVAILABLE_MODELS["transformers_gemma2_9b_cpu"] = lambda: dict(
-    # Note that this model requires an appropriate
-    # HF_TOKEN environment variable
-    name="transformers:google/gemma-2-9b-it",
-    kwargs={
-        "quantization_config": importlib.import_module("transformers").BitsAndBytesConfig(load_in_8bit=True),
-    },
+# Note that this model requires an appropriate HF_TOKEN environment variable
+AVAILABLE_MODELS["transformers_gemma2_9b_cpu"] = lambda: models.Transformers(
+    "google/gemma-2-9b-it",
+    quantization_config=importlib.import_module("transformers").BitsAndBytesConfig(
+        load_in_8bit=True
+    ),
 )
-AVAILABLE_MODELS["transformers_gemma2_9b_gpu"] = lambda: dict(
-    # Note that this model requires an appropriate
-    # HF_TOKEN environment variable
-    name="transformers:google/gemma-2-9b-it",
-    kwargs={
-        "device_map": "cuda:0",
-        "quantization_config": importlib.import_module("transformers").BitsAndBytesConfig(load_in_4bit=True),
-    },
+# Note that this model requires an appropriate HF_TOKEN environment variable
+AVAILABLE_MODELS["transformers_gemma2_9b_gpu"] = lambda: models.Transformers(
+    "google/gemma-2-9b-it",
+    device_map="cuda:0",
+    quantization_config=importlib.import_module("transformers").BitsAndBytesConfig(
+        load_in_4bit=True
+    ),
 )
 
 # GPT 2
-AVAILABLE_MODELS["transformers_gpt2_cpu"] = lambda: dict(name="transformers:gpt2", kwargs=dict())
-AVAILABLE_MODELS["transformers_gpt2_gpu"] = lambda: dict(name="transformers:gpt2", kwargs={"device_map": "cuda:0"})
+AVAILABLE_MODELS["transformers_gpt2_cpu"] = lambda: models.Transformers("gpt2")
+AVAILABLE_MODELS["transformers_gpt2_gpu"] = lambda: models.Transformers(
+    "gpt2", device_map="cuda:0"
+)
 
 # LLAMA 2
-AVAILABLE_MODELS["llamacpp_llama2_7b_cpu"] = lambda: dict(
-    name="huggingface_hubllama:TheBloke/Llama-2-7B-GGUF:llama-2-7b.Q5_K_M.gguf",
-    kwargs={"verbose": True, "n_ctx": 4096},
+AVAILABLE_MODELS["llamacpp_llama2_7b_cpu"] = lambda: models.LlamaCpp(
+    importlib.import_module("huggingface_hub").hf_hub_download(
+        repo_id="TheBloke/Llama-2-7B-GGUF", filename="llama-2-7b.Q5_K_M.gguf"
+    ),
+    verbose=True,
+    n_ctx=4096,
 )
-AVAILABLE_MODELS["llamacpp_llama2_7b_gpu"] = lambda: dict(
-    name="huggingface_hubllama:TheBloke/Llama-2-7B-GGUF:llama-2-7b.Q5_K_M.gguf",
-    kwargs={"verbose": True, "n_gpu_layers": -1, "n_ctx": 4096},
+AVAILABLE_MODELS["llamacpp_llama2_7b_gpu"] = lambda: models.LlamaCpp(
+    importlib.import_module("huggingface_hub").hf_hub_download(
+        repo_id="TheBloke/Llama-2-7B-GGUF", filename="llama-2-7b.Q5_K_M.gguf"
+    ),
+    verbose=True,
+    n_ctx=4096,
+    n_gpu_layers=-1,
 )
 
 # LLAMA 3
-AVAILABLE_MODELS["transformers_llama3_8b_cpu"] = lambda: dict(
-    # Note that this model requires an appropriate
-    # HF_TOKEN environment variable
-    name="transformers:meta-llama/Meta-Llama-3-8B-Instruct",
-    kwargs={"trust_remote_code": True, "torch_dtype": importlib.import_module("torch").bfloat16},
+# Note that this model requires an appropriate HF_TOKEN environment variable
+AVAILABLE_MODELS["transformers_llama3_8b_cpu"] = lambda: models.Transformers(
+    "meta-llama/Meta-Llama-3-8B-Instruct",
+    trust_remote_code=True,
+    torch_dtype=importlib.import_module("torch").bfloat16,
 )
-AVAILABLE_MODELS["transformers_llama3_8b_gpu"] = lambda: dict(
-    # Note that this model requires an appropriate
-    # HF_TOKEN environment variable
-    name="transformers:meta-llama/Meta-Llama-3-8B-Instruct",
-    kwargs={"trust_remote_code": True, "torch_dtype": importlib.import_module("torch").bfloat16, "device_map": "cuda:0"},
+# Note that this model requires an appropriate HF_TOKEN environment variable
+AVAILABLE_MODELS["transformers_llama3_8b_gpu"] = lambda: models.Transformers(
+    "meta-llama/Meta-Llama-3-8B-Instruct",
+    trust_remote_code=True,
+    torch_dtype=importlib.import_module("torch").bfloat16,
+    device_map="cuda:0",
 )
 
 # MISTRAL
-AVAILABLE_MODELS["transformers_mistral_7b_cpu"] = lambda: dict(
-    name="transformers:mistralai/Mistral-7B-v0.1", kwargs=dict()
+AVAILABLE_MODELS["transformers_mistral_7b_cpu"] = lambda: models.Transformers(
+    "mistralai/Mistral-7B-v0.1"
 )
-AVAILABLE_MODELS["llamacpp_mistral_7b_cpu"] = lambda: dict(
-    name="huggingface_hubllama:TheBloke/Mistral-7B-Instruct-v0.2-GGUF:mistral-7b-instruct-v0.2.Q8_0.gguf",
-    kwargs={"verbose": True, "n_ctx": 2048},
+AVAILABLE_MODELS["llamacpp_mistral_7b_cpu"] = lambda: models.LlamaCpp(
+    importlib.import_module("huggingface_hub").hf_hub_download(
+        repo_id="TheBloke/Mistral-7B-Instruct-v0.2-GGUF",
+        filename="mistral-7b-instruct-v0.2.Q8_0.gguf",
+    ),
+    verbose=True,
+    n_ctx=2048,
 )
 
 # PHI 2
-AVAILABLE_MODELS["transformers_phi2_cpu"] = lambda: dict(
-    name="transformers:microsoft/phi-2", kwargs={"trust_remote_code": True}
+AVAILABLE_MODELS["transformers_phi2_cpu"] = lambda: models.Transformers(
+    "microsoft/phi-2", trust_remote_code=True
 )
-AVAILABLE_MODELS["transformers_phi2_gpu"] = lambda: dict(
-    name="transformers:microsoft/phi-2",
-    kwargs={"trust_remote_code": True, "device_map": "cuda:0"},
+AVAILABLE_MODELS["transformers_phi2_gpu"] = lambda: models.Transformers(
+    "microsoft/phi-2", trust_remote_code=True, device_map="cuda:0"
 )
 
 # PHI 3
-AVAILABLE_MODELS["transformers_phi3_mini_4k_instruct_cpu"] = lambda: dict(
-    name="transformers:microsoft/Phi-3-mini-4k-instruct",
-    kwargs={"trust_remote_code": True},
+AVAILABLE_MODELS["transformers_phi3_mini_4k_instruct_cpu"] = lambda: models.Transformers(
+    "microsoft/Phi-3-mini-4k-instruct", trust_remote_code=True
 )
-AVAILABLE_MODELS["llamacpp_phi3_mini_4k_instruct_cpu"] = lambda: dict(
-    name="huggingface_hubllama:microsoft/Phi-3-mini-4k-instruct-gguf:Phi-3-mini-4k-instruct-q4.gguf",
-    kwargs={"verbose": True, "n_ctx": 4096},
+AVAILABLE_MODELS["llamacpp_phi3_mini_4k_instruct_cpu"] = lambda: models.LlamaCpp(
+    importlib.import_module("huggingface_hub").hf_hub_download(
+        repo_id="microsoft/Phi-3-mini-4k-instruct-gguf", filename="Phi-3-mini-4k-instruct-q4.gguf"
+    ),
+    verbose=True,
+    n_ctx=4096,
 )
-AVAILABLE_MODELS["transformers_phi3_small_8k_instruct_gpu"] = lambda: dict(
-    name="transformers:microsoft/Phi-3-small-8k-instruct",
-    kwargs={"trust_remote_code": True, "load_in_8bit": True, "device_map": "cuda:0"},
+AVAILABLE_MODELS["transformers_phi3_small_8k_instruct_gpu"] = lambda: models.Transformers(
+    "microsoft/Phi-3-small-8k-instruct", trust_remote_code=True, load_in_8bit=True, device_map="cuda:0"
 )
 
 # QWEN2DOT5
-AVAILABLE_MODELS["transformers_qwen2dot5_0dot5b_cpu"] = lambda: dict(
-    name="transformers:Qwen/Qwen2.5-0.5B", kwargs=dict()
+AVAILABLE_MODELS["transformers_qwen2dot5_0dot5b_cpu"] = lambda: models.Transformers(
+    "Qwen/Qwen2.5-0.5B"
 )
-AVAILABLE_MODELS["transformers_qwen2dot5_0dot5b_gpu"] = lambda: dict(
-    name="transformers:Qwen/Qwen2.5-0.5B", kwargs={"device_map": "cuda:0"}
+AVAILABLE_MODELS["transformers_qwen2dot5_0dot5b_gpu"] = lambda: models.Transformers(
+    "Qwen/Qwen2.5-0.5B", device_map="cuda:0"
 )
-AVAILABLE_MODELS["transformers_qwen2dot5_0dot5b_instruct_cpu"] = lambda: dict(
-    name="transformers:Qwen/Qwen2.5-0.5B-Instruct", kwargs=dict()
+AVAILABLE_MODELS["transformers_qwen2dot5_0dot5b_instruct_cpu"] = lambda: models.Transformers(
+    "Qwen/Qwen2.5-0.5B-Instruct"
 )
-AVAILABLE_MODELS["transformers_qwen2dot5_0dot5b_instruct_gpu"] = lambda: dict(
-    name="transformers:Qwen/Qwen2.5-0.5B-Instruct", kwargs={"device_map": "cuda:0"}
+AVAILABLE_MODELS["transformers_qwen2dot5_0dot5b_instruct_gpu"] = lambda: models.Transformers(
+    "Qwen/Qwen2.5-0.5B-Instruct", device_map="cuda:0"
 )
 
-
-# Ensure that asserts from tests/utils.py are rewritten by pytest to show helpful messages
-pytest.register_assert_rewrite("tests.utils")
-
-
-from .utils import get_model
 
 def pytest_addoption(parser):
-
     SELECTED_MODEL_ENV_VARIABLE = "GUIDANCE_SELECTED_MODEL"
-
     default_model = os.getenv(SELECTED_MODEL_ENV_VARIABLE, "transformers_gpt2_cpu")
     parser.addoption(
         "--selected_model",
@@ -143,7 +149,7 @@ def selected_model_name(pytestconfig) -> str:
     return pytestconfig.getoption("selected_model")
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="session")
 def selected_model(selected_model_name: str) -> models.Model:
     """Get a concrete model for tests
 
@@ -157,10 +163,7 @@ def selected_model(selected_model_name: str) -> models.Model:
     controlled by the '--selected_model' command
     line argument to pytest.
     """
-    selected_model_info = AVAILABLE_MODELS[selected_model_name]()
-    model = get_model(selected_model_info["name"], **(selected_model_info["kwargs"]))
-    assert model is not None
-    return model
+    return AVAILABLE_MODELS[selected_model_name]()
 
 
 @pytest.fixture(scope="module")
