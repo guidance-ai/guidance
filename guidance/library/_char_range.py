5655d21851a42c051465d9636e5b5826a90e50bf
Hudson Cooper <hncooper96@gmail.com>
2025-02-12 18:24:04 -0800

    remove single-character/single-byte library funcs in favor of regexes

--- guidance/library/_char_range.py
+++
@@ -1,8 +0,0 @@
-from .._grammar import regex
-
-def char_range(low: str, high: str):
-    if len(low) != 1 or len(high) != 1:
-        raise ValueError("char_range only supports single characters")
-    if high < low:
-        raise ValueError("char_range: high must be greater than low")
-    return regex(f"[{low}-{high}]")
--- guidance/library/_char_set.py
+++
@@ -1,9 +0,0 @@
-import re
-from .._grammar import regex
-
-
-def char_set(def_string: str):
-    # TODO: probably should deprecate this function
-    if not re.match(r"(?:\\.|[^\-\\])(?:-(?:\\.|[^\-\\]))?", def_string):
-        raise ValueError("Invalid character set definition (did you want a general regex instead?)")
-    return regex(f"[{def_string}]")
