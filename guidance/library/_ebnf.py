79c06e44c6d46b99d3c4d92c0aeabd6b64f4d5ff
Hudson Cooper <hncooper96@gmail.com>
2025-03-10 09:20:08 -0700

    return-type annotations

--- guidance/library/_ebnf.py
+++ guidance/library/_ebnf.py
@@ -2,7 +2,7 @@ from typing import Optional
 
 from llguidance.gbnf_to_lark import gbnf_to_lark as _gbnf_to_lark
 
-from .._ast import LarkNode
+from .._ast import LarkNode, GrammarNode
 from .._grammar import capture, token_limit, with_temperature
 
 
@@ -12,7 +12,7 @@ def lark(
     name: Optional[str] = None,
     temperature: Optional[float] = None,
     max_tokens: Optional[int] = None,
-):
+) -> GrammarNode:
     """
     Builds a guidance grammar from (a variant of) the EBNF syntax used by the Lark parsing toolkit.
 
@@ -34,7 +34,7 @@ def lark(
     return node
 
 
-def gbnf_to_lark(gbnf_grammar: str):
+def gbnf_to_lark(gbnf_grammar: str) -> str:
     """
     Converts a GBNF (llama.cpp) grammar to Lark(-like) syntax. This is a best-effort
     conversion and may not work for all grammars. We recommend using this function
