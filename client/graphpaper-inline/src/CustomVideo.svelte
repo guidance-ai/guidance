6107328436e4467ec957b09e8f45298c41149c1f
Nicholas King <nicholasking@microsoft.com>
2025-03-10 11:04:39 -0700

    Multimodal widgets (#1156)
    Implements multimodal support in the jupyter widget.
    
    Audio: Custom coded a widget from scratch
    Image: Just shows the image with a standard `<img>` tag
    Video: Uses video.js primarily so that we can customize look and feel of
    controls
    
    Pictures:
    
    <img width="943" alt="image"
    src="https://github.com/user-attachments/assets/85dd6bd2-c64c-4e26-a63f-596dc75dff10"
    />
    
    <img width="730" alt="image"
    src="https://github.com/user-attachments/assets/68d59b1f-b333-4cfd-95e6-365f6b9046a8"
    />
    
    (Video widget not shown because we currently don't have a model that
    supports it)
    
    Thanks to Sam & Hudson for the collaboration to help get this done.
    
    ---------
    
    Co-authored-by: S Jenkins <nopdive@gmail.com>
    Co-authored-by: S Jenkins <69673492+nopdive@users.noreply.github.com>

---
+++ client/graphpaper-inline/src/CustomVideo.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+  import "video.js/dist/video-js.css";
+  import videojs from "video.js";
+  import { onMount, onDestroy } from "svelte";
+  import type { MediaNode } from "./interfaces";
+
+  export let videoData: MediaNode;
+  let videoElement: HTMLVideoElement;
+  let player: any;
+
+  onMount(() => {
+    // Debug log
+    console.log("videoElement exists?", !!videoElement);
+
+    if (videoElement) {
+      // Add a small delay to ensure DOM is ready
+      setTimeout(() => {
+        try {
+          player = videojs(videoElement, {
+            controls: true,
+            fluid: true,
+            playsinline: true,
+            controlBar: {
+                fullscreenToggle: true
+            }
+          });
+          console.log("Player initialized successfully");
+        } catch (e) {
+          console.error("Failed to initialize player:", e);
+        }
+      }, 0);
+    } else {
+      console.error("Video element not found during mount");
+    }
+  });
+
+  onDestroy(() => {
+    if (player) {
+      player.dispose();
+    }
+  });
+</script>
+
+<div class="video-container">
+  <video bind:this={videoElement} class="video-js" playsinline allow="fullscreen" controls>
+    <source src={`data:video/${videoData.format};base64,${videoData.value}`} type="video/mp4" />
+  </video>
+</div>
+
+<style>
+  .video-container {
+    width: 500px; /* Todo: make this more dynamic */
+  }
+</style>
