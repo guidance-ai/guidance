6b2a3caa655c97c7d46b0eb1d3e0dae530d26740
Hudson Cooper <hncooper96@gmail.com>
2025-05-01 14:48:35 -0700

    [Experimental] Support captures with vllm by re-parsing llm output (#1186)
    Seems to work for individual captures. Almost probably breaks for
    list-append

--- guidance/models/experimental/_vllm.py
+++ guidance/models/experimental/_vllm.py
@@ -16,7 +16,22 @@ class VLLMInterpreter(BaseOpenAIInterpreter):
             if isinstance(attr, TextOutput):
                 buffer += attr.value
             yield attr
-        # TODO: apply captures by re-applying parser
+        bts = buffer.encode("utf-8")
+        matches = node.match(bts, raise_exceptions=False)
+        if matches is None:
+            # TODO: should probably raise...
+            # raise ValueError("vLLM failed to constrain the grammar")
+            pass
+        else:
+            for name, value in matches.captures.items():
+                log_probs = matches.log_probs[name]
+                if isinstance(value, list):
+                    assert isinstance(log_probs, list)
+                    assert len(value) == len(log_probs)
+                    for v, l in zip(value, log_probs):
+                        yield self.state.apply_capture(name=name, value=v, log_prob=l, is_append=True)
+                else:
+                    yield self.state.apply_capture(name=name, value=value, log_prob=log_probs, is_append=False)
 
 class VLLMModel(Model):
     def __init__(self, model: str, echo=True, **kwargs):
