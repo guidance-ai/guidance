53eed56f23c3f9bc41c903359eee9aae8eca22bb
Hudson Cooper <hncooper96@gmail.com>
2025-05-01 12:36:36 -0700

    make sure we replace subgrammar token_limit and max_tokens

--- guidance/_grammar.py
+++ guidance/_grammar.py
@@ -142,13 +142,15 @@ def repeat(
 
 def token_limit(value: GrammarNode, max_tokens: int) -> RuleNode:
     """This sets the token limit to be used for the given portion of the grammar."""
-    try:
+    def inner(value: GrammarNode) -> RuleNode:
         if isinstance(value, RuleNode):
             return dataclasses.replace(value, max_tokens=max_tokens)
         else:
             return RuleNode(name="token_limit", value=value, max_tokens=max_tokens)
+    try:
+        return inner(value)
     except ValueError:
-        return RuleNode(name="token_limit", value=subgrammar(value), max_tokens=max_tokens)
+        return inner(subgrammar(value))
 
 
 def with_temperature(value: GrammarNode, temperature: float) -> RuleNode:
@@ -157,13 +159,15 @@ def with_temperature(value: GrammarNode, temperature: float) -> RuleNode:
     Note that if the grammar passed to us already has some portions with a temperature
     setting in place, those settings will not be overridden.
     """
-    try:
+    def inner(value: GrammarNode) -> RuleNode:
         if isinstance(value, RuleNode):
             return dataclasses.replace(value, temperature=temperature)
         else:
             return RuleNode(name="with_temperature", value=value, temperature=temperature)
+    try:
+        return inner(value)
     except ValueError:
-        return RuleNode(name="with_temperature", value=subgrammar(value), temperature=temperature)
+        return inner(subgrammar(value))
 
 
 def capture(value: GrammarNode, name: str, list_append: bool = False) -> RuleNode:
