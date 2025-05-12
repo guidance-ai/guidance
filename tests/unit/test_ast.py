756d82f632c1001ac59942ce22245b5c27d230b1
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2025-05-12 09:33:55 -0400

    Working through tests....

--- tests/unit/test_ast.py
+++ tests/unit/test_ast.py
@@ -1,9 +1,4 @@
-from guidance._ast import (
-    LarkSerializer,
-    LiteralNode,
-    RegexNode,
-    RuleNode,
-)
+from guidance._ast import JoinNode, LarkSerializer, LiteralNode, RegexNode, RuleNode, SelectNode
 
 
 class TestLarkSerializer:
@@ -20,13 +15,48 @@ START: "A"
 """
         assert result == expected
 
+    def test_select_literals(self):
+        target = LarkSerializer()
+        la = LiteralNode("A")
+        lb = LiteralNode("B")
+
+        sn = SelectNode((la, lb))
+
+        result = target.serialize(sn)
+        print(result)
+
+        expected = """%llguidance {}
+
+start: START
+START: "A"
+     | "B"
+
+"""
+
+        assert result == expected
+
+    def test_join_literals(self):
+        target = LarkSerializer()
+        la = LiteralNode("A")
+        lb = LiteralNode("B")
+
+        jn = JoinNode((la, lb))
+
+        result = target.serialize(jn)
+        print(result)
+        expected = """%llguidance {}
+
+start: START
+START: "A" "B"
+"""
+        assert result == expected
+
     def test_capture_rule_node(self):
         target = LarkSerializer()
         ren = RegexNode(".*")
         rule_node = RuleNode("my_rule", value=ren, capture="my_capture")
 
         result = target.serialize(rule_node)
-
         print(result)
 
         expected = """%llguidance {}
