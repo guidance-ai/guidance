737864e649f89e6e21499c376e23bff08bc3b40c
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2025-05-12 09:25:37 -0400

    Another test

--- tests/unit/test_ast.py
+++ tests/unit/test_ast.py
@@ -1,11 +1,25 @@
 from guidance._ast import (
     LarkSerializer,
+    LiteralNode,
     RegexNode,
     RuleNode,
 )
 
 
 class TestLarkSerializer:
+    def test_smoke(self):
+        target = LarkSerializer()
+        ln = LiteralNode("A")
+
+        result = target.serialize(ln)
+        print(result)
+        expected = """%llguidance {}
+
+start: START
+START: "A"
+"""
+        assert result == expected
+
     def test_capture_rule_node(self):
         target = LarkSerializer()
         ren = RegexNode(".*")
