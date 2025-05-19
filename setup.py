45ce2e889526ed348940e1ffa1b523bc8403697e
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2025-05-19 09:02:44 -0400

    Bump llamacpp

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-llamacpp_requires = ["llama-cpp-python==0.3.7"]
+llamacpp_requires = ["llama-cpp-python==0.3.9"]
 transformers_requires = ["transformers==4.48.2"]
 
 install_requires = [
