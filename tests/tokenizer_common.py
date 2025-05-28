guidance-ai/guidance

commit 534210f7347138c11beaff6fb6e58910a9d3abc1

[Refactor] Use `llguidance`'s native llamacpp tokenizers  (#1218)
Simplify llamacpp implementation by using llguidance's new support for native llamacpp tokenizers

setup.py
@@ -28,7 +28,7 @@ install_requires = [
     "requests",
     "psutil",
     "guidance-stitch",
-    "llguidance==0.7.22",
+    "llguidance==0.7.24",
 ]
 
 # Our basic list of 'extras'

guidance/models/_llama_cpp.py
@@ -5,8 +5,7 @@ import os
 import sys
 from itertools import takewhile
 from pathlib import Path
-from typing import TYPE_CHECKING, Sequence, Union
-from collections import Counter
+from typing import TYPE_CHECKING, Union
 import ctypes
 
 import numpy as np
@@ -15,8 +14,7 @@ from .._schema import GenToken, GenTokenExtra
 from .._utils import normalize_notebook_stdout_stderr, softmax
 from ..chat import ChatTemplate
 from ._base import Model
-from ._engine import Engine, EngineInterpreter, EngineState, Tokenizer
-from ._engine._tokenizer import TokenizerWrappable
+from ._engine import Engine, EngineInterpreter, Tokenizer
 
 try:
     import llama_cpp
@@ -24,11 +22,12 @@ try:
     is_llama_cpp = True
 except ModuleNotFoundError:
     is_llama_cpp = False
+else:
+    import llguidance.llamacpp
 
 if TYPE_CHECKING:
     from llama_cpp.llama_tokenizer import LlamaTokenizer
     from llama_cpp.llama import Llama
-    from llama_cpp.llama_chat_format import Jinja2ChatFormatter
 
 logger = logging.getLogger(__name__)
 
@@ -89,32 +88,10 @@ class LlamaCppTokenizer(Tokenizer):
     def __init__(self, model_obj: "Llama", chat_template: Union[str, ChatTemplate, None] = None):
         self._model_obj = model_obj
 
-        tokenizer = llama_cpp.LlamaTokenizer(model_obj)
         vocab = llama_cpp.llama_model_get_vocab(model_obj.model)
         if vocab is None:
             raise Exception("call to llama_cpp.llama_model_get_vocab returned NULL.")
-
-        # get the bytes strings for all the tokens
-        tokens: list[bytes] = []
-        special_token_ids: list[int] = []
-        for i in range(tokenizer._model.n_vocab()):
-            tok_attrs = tokenizer._model.token_get_attr(i)
-            if tok_attrs & llama_cpp.LLAMA_TOKEN_ATTR_CONTROL:
-                special_token_ids.append(i)
-            tok = detokenize(tokenizer, [i], special=True, size=256)
-            tokens.append(tok)
-
-        # Search for a byte string that prefixes exactly one token
-        first_byte_counts = Counter(tok[0:1] for tok in tokens)
-        for candidate in (b"\x00", b"\x01", b"\x02", b"\x03"):
-            if first_byte_counts[candidate] == 1:
-                self._sentinel_bytes = candidate
-                break
-        else:
-            raise ValueError("Could not find a sentinel byte for tokenizer")
-        self._sentinel_tokens = self._model_obj.tokenize(
-            self._sentinel_bytes, add_bos=False, special=True
-        )
+        ll_tokenizer = llguidance.llamacpp.lltokenizer_from_vocab(vocab)
 
         # Chat Template logic
         if chat_template is None:
@@ -124,31 +101,11 @@ class LlamaCppTokenizer(Tokenizer):
             ):
                 chat_template = self._model_obj.metadata["tokenizer.chat_template"]
 
-        ll_tokenizer = TokenizerWrappable(
-            eos_token_id=tokenizer._model.token_eos(),
-            bos_token_id=tokenizer._model.token_bos(),
-            tokens=tokens,
-            special_token_ids=special_token_ids,
-            # ENCODE MUST BE OVERRIDDEN
-            encode_callable=self.encode
-        ).as_ll_tokenizer()
-
         super().__init__(
             ll_tokenizer=ll_tokenizer,
             chat_template=chat_template,
-            bos_token_id=tokenizer._model.token_bos()
+            bos_token_id=model_obj.token_bos()
         )
-
-    def encode(self, byte_string: bytes) -> list[int]:
-        # Workaround for the LlamaCpp prepending spaces on encoding
-        raw_tokens = self._model_obj.tokenize(
-            self._sentinel_bytes + byte_string, add_bos=False, special=True
-        )
-        if raw_tokens[: len(self._sentinel_tokens)] != self._sentinel_tokens:
-            raise ValueError(
-                f"Failed to tokenize {byte_string} using sentinel bytes {self._sentinel_bytes}."
-            )
-        return raw_tokens[len(self._sentinel_tokens) :]
 
 
 class LlamaCppEngine(Engine):

commit ce57e15d64c4ef5655fe646a28ce8b6c5f4ee5db

[Bugfix] Ensure special tokens are tokenized as such (broken by #1218) (#1224)
Fixes regression introduced by #1218, where the llamacpp tokenizer fails
to parse special tokens. Fixed by adding behavior and corresponding
kwarg to llguidance, exposed here (with default True).

Added explicit test for this, as our existing tests did not cover this
behavior!

guidance/models/_engine/_tokenizer.py
@@ -69,9 +69,9 @@ class Tokenizer:
     def __call__(self, byte_string: bytes):
         return self.encode(byte_string)
 
-    def encode(self, byte_string: bytes) -> list[int]:
+    def encode(self, byte_string: bytes, *, parse_special: bool = True) -> list[int]:
         """Returns a list of tokens that represent the given byte string."""
-        return self._ll_tokenizer.tokenize_bytes(byte_string)
+        return self._ll_tokenizer.tokenize_bytes(byte_string, parse_special=parse_special)
 
     def decode(self, tokens: Sequence[int]) -> bytes:
         """Returns the bytes represented by the given list of tokens."""

setup.py
@@ -28,7 +28,7 @@ install_requires = [
     "requests",
     "psutil",
     "guidance-stitch",
-    "llguidance==0.7.24",
+    "llguidance==0.7.25",
 ]
 
 # Our basic list of 'extras'

tests/model_integration/test_tokenizers.py
@@ -33,3 +33,7 @@ class TestUnauthenticatedTransformerTokenizers(BaseTestTransformerTokenizers):
     @pytest.mark.parametrize("target_string", TOKENIZER_ROUND_TRIP_STRINGS)
     def test_string_roundtrip(self, model_name: str, target_string: str):
         self.base_string_roundtrip(model_name, target_string)
+
+    @pytest.mark.parametrize("model_name", TRANSFORMER_MODELS)
+    def test_eos_bos_token_round_trip(self, model_name: str):
+        self.base_eos_bos_token_round_trip(model_name)

tests/model_specific/llama_cpp_tests/test_llama_cpp.py
@@ -231,3 +231,13 @@ class TestLlamaCppTokenizers:
         final_string = decoded.decode()
 
         assert final_string == target_string
+
+    def test_eos_bos_token_round_trip(self, llamacpp_model: guidance.models.LlamaCpp):
+        my_tok = llamacpp_model.engine.tokenizer
+
+        assert my_tok.eos_token == my_tok.decode([my_tok.eos_token_id])
+        assert my_tok.encode(my_tok.eos_token) == [my_tok.eos_token_id]
+
+        if my_tok.bos_token is not None:
+            assert my_tok.bos_token == my_tok.decode([my_tok.bos_token_id])
+            assert my_tok.encode(my_tok.bos_token) == [my_tok.bos_token_id]

tests/tokenizer_common.py
@@ -32,3 +32,18 @@ class BaseTestTransformerTokenizers:
         final_string = decoded.decode()
 
         assert final_string == target_string
+
+    def base_eos_bos_token_round_trip(
+        self, model_name: str
+    ):
+        my_tok = models.TransformersTokenizer.from_pretrained(
+            model_name,
+            trust_remote_code=True,
+        )
+
+        assert my_tok.eos_token == my_tok.decode([my_tok.eos_token_id])
+        assert my_tok.encode(my_tok.eos_token) == [my_tok.eos_token_id]
+
+        if my_tok.bos_token is not None:
+            assert my_tok.bos_token == my_tok.decode([my_tok.bos_token_id])
+            assert my_tok.encode(my_tok.bos_token) == [my_tok.bos_token_id]