cf157fc230fc2a7cf03153c08c17adf574f5f924
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2025-05-29 09:06:28 -0400

    Better path, I think?

--- tests/notebooks/test_notebooks.py
+++ tests/notebooks/test_notebooks.py
@@ -15,7 +15,7 @@ BASE_NB_PATH = pathlib.Path("./notebooks").absolute()
 def run_notebook(notebook_path: pathlib.Path, params: Optional[Dict[str, Any]] = None):
     assert notebook_path.exists(), f"Checking for: {notebook_path}"
     output_nb = notebook_path.stem + ".papermill_out" + notebook_path.suffix
-    output_path = TestTutorials.BASE_TUTORIAL_PATH / output_nb
+    output_path = notebook_path.parent / output_nb
 
     # Just make sure nothing throws an exception
     pm.execute_notebook(input_path=notebook_path, output_path=output_path, parameters=params)
