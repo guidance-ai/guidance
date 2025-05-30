92828eb8ce32415f10e0994e2da691260b261dbb
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2025-05-30 12:59:00 -0400

    Some assertions for a test

--- tests/model_integration/test_roles.py
+++ tests/model_integration/test_roles.py
@@ -1,39 +1,70 @@
 import guidance
 from guidance import assistant, user, system, models, gen
 
-
 @guidance
 def plan_for_goal(lm, goal: str):
-    # This is condensed out of the chat.ipynb notebook
-
+    
     # Some general instruction to the model
     with system():
         lm += "You are a helpful assistant."
 
-    LM_EXPECT = "<|im_start|>system\nYou are a helpful assistant."
-    assert str(lm) == LM_EXPECT, f"{str(lm)=}"
+    
+    EXPECTED_LM = "<|im_start|>system\nYou are a helpful assistant."
+    print(f"{str(lm)=}")
+    assert str(lm) == EXPECTED_LM
 
+    # Simulate a simple request from the user
+    # Note that we switch to using 'lm2' here, because these are intermediate steps (so we don't want to overwrite the current lm object)
     with user():
-        lm2 = lm + f"""I want to {goal}. Make some very short suggestions"""
+        lm2 = lm + f"""\
+        I want to {goal}
+        Can you please generate one option for how to accomplish this?
+        Please make the option very short, at most one line."""
 
-    assert str(lm) == "<|im_start|>system\nYou are a helpful assistant.", f"{str(lm)=}"
-    LM2_EXPECT = "<|im_start|>system\nYou are a helpful assistant.<|im_end|>\n<|im_start|>user\nI want to Add numbers. Make some very short suggestions"
-    assert str(lm2) == LM2_EXPECT, f"{str(lm2)=}"
+    EXPECTED_LM2 = "<|im_start|>system\nYou are a helpful assistant.<|im_end|>\n<|im_start|>user\nI want to Add numbers\nCan you please generate one option for how to accomplish this?\nPlease make the option very short, at most one line."
+    print(f"{str(lm)=}")
+    print(f"{str(lm2)=}")
+    assert str(lm) == EXPECTED_LM
+    assert str(lm2) == EXPECTED_LM2
 
-    n_opts = 2
-    options = []
+    # Generate several options. Note that this means several sequential generation requests
+    n_options = 2
     with assistant():
-        for _ in range(n_opts):
-            lm3 = lm2 + gen(name="option", temperature=1.0, max_tokens=50)
-            LM3_BASE = "<|im_start|>system\nYou are a helpful assistant.<|im_end|>\n<|im_start|>user\nI want to Add numbers. Make some very short suggestions<|im_end|>\n<|im_start|>assistant\n"
-            selected = lm3["option"]
-            assert selected == "something", f"{selected=}"
-            assert str(lm3) == LM3_BASE + selected, f"{str(lm3)=}"
-            assert str(lm2) == LM2_EXPECT, f"{str(lm2)=}"
-            options.append(selected)
-    assert len(options) == 2
+        options = []
+        for i in range(n_options):
+            lm3 = lm2 + gen(name='option', temperature=1.0, max_tokens=50)
+            print(f"{str(lm3)=}")
+            print("--")
+            nxt_opt = lm3["option"]
+            print(f"{nxt_opt=}")
+            assert "<|" not in nxt_opt
+            options.append(nxt_opt)
+            print("------")
 
+
+    print(f"{str(lm)=}")
+    print(f"{str(lm2)=}")
+    print("==\n")
+
+    # Have the user request pros and cons
+    with user():
+        lm2 += f"""\
+        I want to {goal}
+        Can you please comment on the pros and cons of each of the following options, and then pick the best option?
+        ---
+        """
+        for i, opt in enumerate(options):
+            lm2 += f"Option {i}: {opt}\n"
+        lm2 += f"""\
+        ---
+        Please discuss each option very briefly (one line for pros, one for cons), and end by saying Best=X, where X is the number of the best option."""
+
+    # Get the pros and cons from the model
+    with assistant():
+        lm2 += gen(name='prosandcons', temperature=0.0, max_tokens=600, stop="Best=") + "Best=" + gen("best", regex="[0-9]+")
+    
     return lm
+
 
 
 def test_plan_for_goal(chat_model: models.Model):
