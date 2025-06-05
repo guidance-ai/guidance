4213a76221dd9ab1f552661570eb0671e2bd9b8f
Hudson Cooper <hncooper96@gmail.com>
2025-06-05 15:11:13 -0700

    roughly cut out old tools and use new tool call ast

--- guidance/library/__init__.py
+++ guidance/library/__init__.py
@@ -1,5 +1,5 @@
 # import functions that can be called directly
-from ._gen import gen, call_tool, regex
+from ._gen import gen, regex
 from ._image import image, gen_image
 from ._audio import audio, gen_audio
 from ._video import video, gen_video
@@ -21,6 +21,5 @@ from ._role import role, system, assistant, user #, function, instruction, inden
 from ._sequences import one_or_more, zero_or_more, at_most_n_repeats, exactly_n_repeats, sequence
 from ._substring import substring
 from ._optional import optional
-from ._tool import Tool
 from ._json import json
 from ._ebnf import lark, gbnf_to_lark
