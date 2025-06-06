guidance-ai/guidance

commit 114ff7e5087495711a54d7f111dfbf2ac00d9ea8

[Feature] Support schemaless json (esp. for remote endpoints) (#1245)
Allows `JsonNode` to have `None` schema (e.g. `lm += json()` with no
schema passed), which we can pass along to remote providers (e.g. for
[openai](https://platform.openai.com/docs/guides/structured-outputs/introduction?api-mode=chat#json-mode)).

For llguidance-enabled backends, we can pass the schema `{"type":
"object"}`, as schemaless json mode seems to generally mean "any json
OBJECT" rather than "any json INSTANCE".

Note that `lm += json(schema=True)` has the semantics of `lm +=
json(schema={})`, i.e. "any json INSTANCE". Most non-llguidance backends
won't be able to support this.

guidance/library/_json.py
@@ -1,12 +1,9 @@
-import warnings
-from json import dumps as json_dumps
 from json import loads as json_loads
-from typing import Any, Mapping, Optional, Type, Union, cast
+from typing import Any, Mapping, Optional, Type, Union
 
 import pydantic
-from llguidance import LLMatcher
 
-from .._ast import JsonNode, RuleNode
+from .._ast import JsonNode, LLGJsonCompileOptions
 from .._grammar import capture, token_limit, with_temperature
 from ._pydantic import pydantic_to_json_schema
 
@@ -21,7 +18,7 @@ def json(
         str,
         JSONSchema,
         Type[pydantic.BaseModel],
-        pydantic.TypeAdapter,
+        pydantic.TypeAdapter[Any],
     ] = None,
     temperature: Optional[float] = None,
     max_tokens: Optional[int] = None,
@@ -76,10 +73,7 @@ def json(
     if schema is False:
         raise ValueError("Unsatisfiable schema: schema is false")
     elif schema is True:
-        schema = {}
-    elif schema is None:
-        # Default schema is empty, "anything goes" schema
-        # TODO: consider default being `{"type": "object"}`
+        # Any valid JSON is acceptable
         schema = {}
     elif isinstance(schema, pydantic.TypeAdapter) or (
         isinstance(schema, type) and issubclass(schema, pydantic.BaseModel)
@@ -91,11 +85,6 @@ def json(
             raise ValueError("JSON schema string must be a JSON object (i.e. a dictionary)")
         schema = from_str
 
-    if isinstance(schema, Mapping):
-        schema = dict(schema)
-    else:
-        raise TypeError(f"Unsupported schema type: {type(schema)}")
-
     if whitespace_pattern is not None:
         whitespace_flexible = True
 
@@ -104,40 +93,39 @@ def json(
             separators = (",", ":")
         else:
             separators = (", ", ": ")
+
     if len(separators) != 2:
         raise ValueError("separators must be a tuple of (item_separator, key_separator)")
     item_separator, key_separator = separators
 
-    if schema.get("x-guidance") is None:
-        schema["x-guidance"] = {
-            "item_separator": item_separator,
-            "key_separator": key_separator,
-            "whitespace_flexible": whitespace_flexible,
-            "whitespace_pattern": whitespace_pattern,
-            "coerce_one_of": True,
-            "lenient": lenient,
-        }
+    llg_options = LLGJsonCompileOptions(
+        whitespace_flexible=whitespace_flexible,
+        whitespace_pattern=whitespace_pattern,
+        item_separator=item_separator,
+        key_separator=key_separator,
+        coerce_one_of=True,
+        lenient=lenient,
+    )
 
-    # TODO: decide whether or not to keep this -- it lets us double check that llguidance can handle the schema which isn't necessarily
-    # what we want, as llguidance may or may not be the backend we are using. That being said, it's sort of nice to get an exception when
-    # you call `json` instead of waiting for generation to fail.
+    if isinstance(schema, Mapping):
+        schema = dict(schema)
+    elif schema is not None:
+        raise TypeError(
+            f"Invalid schema type: {type(schema)}. Expected None, a boolean, a JSON schema object, a pydantic model, or a pydantic TypeAdapter."
+        )
+
+    node = JsonNode(
+        schema=schema,
+        llg_options=llg_options,
+    )
+
     VALIDATE = True
     if VALIDATE:
-        grm = LLMatcher.grammar_from_json_schema(schema)
-        is_err, messages = LLMatcher.validate_grammar_with_warnings(grm)
-        if is_err:
-            raise ValueError(messages[0])
-        else:
-            # this will warn about oneOf coercion, and any other unsupported features if lenient is enabled
-            for message in messages:
-                warnings.warn(message)
+        # TODO: decide whether or not to keep this -- it lets us double check that llguidance can handle the schema which isn't necessarily
+        # what we want, as llguidance may or may not be the backend we are using. That being said, it's sort of nice to get an exception when
+        # you call `json` instead of waiting for generation to fail.
+        node._llguidance_validate()
 
-    node = RuleNode(
-        name=name or "json",
-        value=JsonNode(
-            schema=schema,
-        )
-    )
     if temperature is not None:
         node = with_temperature(node, temperature)
     if max_tokens is not None:

guidance/_ast.py
@@ -1,6 +1,7 @@
 import json
 import re
 import textwrap
+import copy
 from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
 from typing import (
@@ -12,9 +13,13 @@ from typing import (
     Sequence,
     TypeVar,
     Union,
+    TypedDict,
     cast,
 )
 from typing_extensions import assert_never
+from functools import cached_property
+from llguidance import LLMatcher
+import warnings
 
 from ._parser import ByteParser, ByteParserException
 from .trace import OutputAttr
@@ -514,14 +519,55 @@ class SubgrammarNode(BaseSubgrammarNode):
     def _run(self, interpreter: "Interpreter[S]", **kwargs) -> Iterator[OutputAttr]:
         return interpreter.subgrammar(self, **kwargs)
 
+class LLGJsonCompileOptions(TypedDict):
+    # defaults to ","
+    item_separator: Optional[str]
+    # defaults to ":"
+    key_separator: Optional[str]
+    # defaults to None - depends on whitespace_flexible
+    whitespace_pattern: Optional[str]
+    # defaults to true (r"[\x20\x0A\x0D\x09]+"); if false, no whitespace is allowed
+    whitespace_flexible: Optional[bool]
+    # defaults to false
+    coerce_one_of: Optional[bool]
+    # ignore unimplemented keywords; defaults to false
+    lenient: Optional[bool]
 
 @dataclass(frozen=True, eq=False)
 class JsonNode(BaseSubgrammarNode):
-    schema: dict[str, Any]
+    schema: Optional[dict[str, Any]] = None
+    llg_options: Optional[LLGJsonCompileOptions] = None
 
     def _run(self, interpreter: "Interpreter[S]", **kwargs) -> Iterator[OutputAttr]:
         return interpreter.json(self, **kwargs)
 
+    @cached_property
+    def _llguidance_json(self) -> dict[str, Any]:
+        if self.schema is None:
+            # The user did not pass a schema. Let's assume that they want an object
+            # (this should match the behavior of most remote providers)
+            schema = {"type": "object"}
+        else:
+            # shallow copy is ok
+            schema = copy.copy(self.schema)
+
+        if self.llg_options is not None:
+            # Maybe TODO: let LLGJsonCompileOptions be non-total
+            # and update the schema with any present options
+            # (in case x-guidance was already set with some options)
+            schema["x-guidance"] = self.llg_options
+        return schema
+    
+    def _llguidance_validate(self) -> None:
+        """Validate the JSON schema with `llguidance` and warn about any issues."""
+        grm = LLMatcher.grammar_from_json_schema(self._llguidance_json)
+        is_err, messages = LLMatcher.validate_grammar_with_warnings(grm)
+        if is_err:
+            raise ValueError(messages[0])
+        else:
+            # this will warn about oneOf coercion, and any other unsupported features if lenient is enabled
+            for message in messages:
+                warnings.warn(message)
 
 @dataclass(frozen=True, eq=False)
 class LarkNode(BaseSubgrammarNode):
@@ -600,7 +646,7 @@ class LarkSerializer:
             res += ": "
             target = node.value
             if isinstance(target, JsonNode):
-                res += "%json " + json.dumps(target.schema, indent=2)
+                res += "%json " + json.dumps(target._llguidance_json, indent=2)
             elif isinstance(target, LarkNode):
                 # TODO: we can't decide whether or not to enforce max tokens here easily.
                 # We could in principle parse the grammar and/or use a regex?

guidance/library/_pydantic.py
@@ -26,7 +26,7 @@ class GenerateJsonSchemaSafe(pydantic.json_schema.GenerateJsonSchema):
 
 
 def pydantic_to_json_schema(
-    schema: Union[Type["pydantic.BaseModel"], "pydantic.TypeAdapter"]
+    schema: Union[Type["pydantic.BaseModel"], "pydantic.TypeAdapter[Any]"]
 ) -> Dict[str, Any]:
     if inspect.isclass(schema) and issubclass(schema, pydantic.BaseModel):
         return schema.model_json_schema(schema_generator=GenerateJsonSchemaSafe)

guidance/models/_openai_base.py
@@ -393,15 +393,19 @@ class OpenAIRegexMixin(BaseOpenAIInterpreter):
 
 class OpenAIJSONMixin(BaseOpenAIInterpreter):
     def json(self, node: JsonNode, **kwargs) -> Iterator[OutputAttr]:
-        return self._run(
+        if node.schema is None:
+            response_format = {"type": "json_object"}
+        else:
             response_format={
                 "type": "json_schema",
                 "json_schema": {
                     "name": "json_schema",  # TODO?
-                    "schema": {k: v for k,v in node.schema.items() if k != "x-guidance"},
+                    "schema": node.schema,
                     "strict": True,
                 },
             },
+        return self._run(
+            response_format=response_format,
             **kwargs,
         )
 

commit 82edd3085381c5e8a14e8b229424bc4e8167e064

[Hotfix] JsonNode must be wrapped in RuleNode (#1248)
A quirk in the serialization code... we have no tests of this edge case,
as it is only hit when there's no capture name / temperature / etc.

Introduced regression in #1245

guidance/library/_json.py
@@ -3,7 +3,7 @@ from typing import Any, Mapping, Optional, Type, Union
 
 import pydantic
 
-from .._ast import JsonNode, LLGJsonCompileOptions
+from .._ast import JsonNode, LLGJsonCompileOptions, RuleNode
 from .._grammar import capture, token_limit, with_temperature
 from ._pydantic import pydantic_to_json_schema
 
@@ -126,10 +126,15 @@ def json(
         # you call `json` instead of waiting for generation to fail.
         node._llguidance_validate()
 
+    rule = RuleNode(
+        name=name or "json",
+        value=node,
+    )
+
     if temperature is not None:
-        node = with_temperature(node, temperature)
+        rule = with_temperature(rule, temperature)
     if max_tokens is not None:
-        node = token_limit(node, max_tokens)
+        rule = token_limit(rule, max_tokens)
     if name is not None:
-        node = capture(node, name)
-    return node
+        rule = capture(rule, name)
+    return rule