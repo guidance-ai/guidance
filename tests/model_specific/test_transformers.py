fbd46e3b3081ea2ccc05f014bbc1c65fed4cfbff
Hudson Cooper <hncooper96@gmail.com>
2025-06-16 10:03:14 -0400

    add equivalent test for transformers

--- tests/model_specific/test_transformers.py
+++ tests/model_specific/test_transformers.py
@@ -1,4 +1,5 @@
 import pytest
+import jinja2
 
 from guidance import gen, select, models, assistant, system, user, guidance
 
@@ -197,3 +198,52 @@ def test_phi3_basic_completion_badtokens(phi3_model: models.Model):
     lm += gen("five", max_tokens=10)
 
     assert len(lm["five"]) > 0
+
+def test_chat_format_smoke(selected_model):
+    # Retrieve the template string
+    if isinstance(selected_model.engine.tokenizer._orig_tokenizer.chat_template, str):
+        model_chat_template = selected_model.engine.tokenizer._orig_tokenizer.chat_template
+    else:
+        pytest.skip("Chat template not available from Transformers object")
+
+    messages = [
+        {"role": "user", "content": "Good_day_to_you!"},
+        {"role": "assistant", "content": "Hello!"},
+    ]
+
+    # Note that llama-cpp-python does provide a llama_chat_apply_template function
+    # but details about its use are thin on the ground and according to
+    # https://github.com/ggerganov/llama.cpp/wiki/Templates-supported-by-llama_chat_apply_template
+    # it does its own thing internally
+    jinja2_template = jinja2.Environment(loader=jinja2.BaseLoader()).from_string(
+        model_chat_template
+    )
+    jinja2_render = jinja2_template.render(
+        messages=messages,
+        bos_token=selected_model.engine.tokenizer.bos_token.decode(),
+        eos_token=selected_model.engine.tokenizer.eos_token.decode(),
+    )
+
+    lm = selected_model
+    with user():
+        lm += "Good_day_to_you!"
+    with assistant():
+        lm += "Hello!"
+
+    # Compare the tokenization of the strings, rather than the strings
+    # themselves (e.g. `<|user|>` may tokenize the same as `<|user|>\n`)
+    lm_tokens = lm._interpreter.engine.tokenizer.encode(
+        str(lm).encode()
+    )
+    jinja2_tokens = lm._interpreter.engine.tokenizer.encode(
+        jinja2_render.encode()
+    )
+
+    # Only check substring due to BOS/EOS tokens, unfinished closing tags
+    diff = len(jinja2_tokens) - len(lm_tokens)
+    assert diff >= 0
+    for i in range(diff+1):
+        if jinja2_tokens[i:i+len(lm_tokens)] == lm_tokens:
+            break
+    else:
+        raise AssertionError("lm mismatches jinja template", str(lm), str(jinja2_render))
