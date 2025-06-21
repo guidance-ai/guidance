guidance-ai/guidance

commit ee88c86cc057c286f15f22c2246c807e9831e1d6

Add probabilities back in for ff_tokens (#1261)
Computes the probabilities of fast-forwarded tokens (in addition to
generated tokens, for which we already had probabilities).

To implement this, I implemented a "logit cache" for the
`TransformersEngine` and `LlamacppEngine` classes, which maintains a
cached version of all `(seq_len, vocab_size)` logits that have been
computed thus far. Each call into the underlying model object will
(possibly) truncate and then append to the cache (i.e. it has the same
semantics as the KV cache, but its size should be pretty negligable in
comparison).

There is a slight downside -- we need to call the model one extra time
at the end of generation IF the generation ended with fast-forwarded
tokens. We need to do this in order to get their probabilities. Another
note here, but I'm currently caching the logits as numpy arrays. In
principle, it might be nice to keep torch tensors on their original
device... All of the round trips might be kind of expensive. That being
said, this issue was already present in the previous implementation.

It would be great to follow this PR up with something that gets the
top-k tokens for ff tokens too.

guidance/models/_engine/_engine.py
@@ -7,6 +7,7 @@ from abc import ABC, abstractmethod
 from typing import TYPE_CHECKING, Callable, Iterator, Optional
 
 import numpy as np
+from numpy.typing import NDArray
 
 from ..._parser import TokenParser
 from ..._schema import (
@@ -76,12 +77,11 @@ class Engine(ABC):
     Server so a single server can serve many clients' model objects through a single Engine object.
     """
 
-    def __init__(self, tokenizer: Tokenizer, compute_log_probs=False, enable_backtrack=True, enable_ff_tokens=True,
+    def __init__(self, tokenizer: Tokenizer, enable_backtrack=True, enable_ff_tokens=True,
                  enable_monitoring=True, **kwargs):
         from ...registry import get_monitor
 
         self.tokenizer = tokenizer
-        self.compute_log_probs = compute_log_probs
         self._enable_backtrack = enable_backtrack
         self._enable_ff_tokens = enable_ff_tokens
         self._enable_monitoring = enable_monitoring
@@ -165,8 +165,8 @@ class Engine(ABC):
             enable_ff_tokens=self.enable_ff_tokens,
         )
 
+        last_temperature = 1.0
         engine_output = None
-        logits_lat_ms = 0
         while not parser.done():
             t0 = time.time()
 
@@ -197,18 +197,26 @@ class Engine(ABC):
                 # (model + prompt) + grammar == model + (prompt + grammar)
                 tokens = self.tokenizer.recode(tokens)
 
-            # Note that has_pending_stop implies that the response is a stop response,
-            # but the converse is not true. We can therefore avoid some (but not all)
-            # unnecessary calls to get_logits on the final iteration.
-            has_pending_stop = parser.has_pending_stop()
-
-            if not has_pending_stop:
-                t1 = time.time()
-                logits = self.get_logits(token_ids=tokens)
-                logits_lat_ms = (time.time() - t1) * 1000
-            else:
-                # Avoid calling get_logits if we know we won't use it
+            # We can avoid a final get_logits call in the case that:
+            # 1. The parser has a pending stop
+            # 2. There are no ff_tokens (except for our last generated token)
+            # TODO: allow avoiding final forward pass if metrics are disabled
+            # and we have a pending stop
+            if parser.has_pending_stop() and (
+                (not ff_tokens)
+                or (
+                    len(ff_tokens) == 1
+                    and engine_output is not None
+                    and ff_tokens[0] == engine_output.issued_token.token_id
+                )
+            ):
                 logits = None
+                logits_lat_ms = 0.0
+            else:
+                t1 = time.time()
+                logits = self.get_logits(token_ids=tokens, full_sequence=True)
+                logits_lat_ms = (time.time() - t1) * 1000
+
 
             # Important: don't wait on this future until after getting the logits;
             # this allows the mask to be built concurrently with model inference
@@ -221,13 +229,31 @@ class Engine(ABC):
                 # if it exists
                 ff_lat_ms -= logits_lat_ms
 
+            if logits is not None:
+                # Not the last one -- that's for the *next* token.
+                ff_logits = logits[max(len(logits)-len(ff_tokens)-1, 0):-1]
+                ff_probs = (
+                    softmax(np.array(ff_logits))
+                    if last_temperature < 0.0001
+                    else softmax(np.array(ff_logits) / last_temperature)
+                )
+                if ff_probs.shape[0] != len(ff_tokens):
+                    assert ff_probs.shape[0] == len(ff_tokens) - 1
+                    # We didn't have a BOS token, so we need to fake the first token prob (say... 1?)
+                    ff_probs = np.concatenate(
+                        (np.ones((1, ff_probs.shape[1])), ff_probs), axis=0
+                    )
+            else:
+                ff_probs = np.empty(shape=())
+
             gen_tokens = []
             if engine_output is None:
-                for token_id in ff_tokens:
+                for i, token_id in enumerate(ff_tokens):
                     gen_tokens.append(
                         GenToken(
                             token_id=token_id,
                             bytes=self.tokenizer.decode([token_id]),
+                            prob=ff_probs[i, token_id],
                             # amortize latency
                             latency_ms=ff_lat_ms/len(ff_tokens),
                             is_input=True,
@@ -240,11 +266,12 @@ class Engine(ABC):
                     ff_start_index = 0
                 else:
                     ff_start_index = 1
-                for token_id in ff_tokens[ff_start_index:]:
+                for i, token_id in enumerate(ff_tokens[ff_start_index:], start=ff_start_index):
                     gen_tokens.append(
                         GenToken(
                             token_id=token_id,
                             bytes=self.tokenizer.decode([token_id]),
+                            prob=ff_probs[i, token_id],
                             # amortize latency
                             latency_ms=ff_lat_ms/len(ff_tokens[ff_start_index:]),
                             is_force_forwarded=True,
@@ -272,10 +299,8 @@ class Engine(ABC):
                 # Ensure we break AFTER yielding the final response
                 break
 
-            # If there was a pending stop, we should have broken out of the loop
-            assert not has_pending_stop
-
             # Help the type checker: assert that everything we need to get the next token is not None
+            assert logits is not None
             assert mask is not None
             assert ll_response.temperature is not None
 
@@ -294,7 +319,7 @@ class Engine(ABC):
                 mask_for_sampling = mask
 
             engine_output = self.get_next_token_with_top_k(
-                logits=logits,
+                logits=logits[-1, :],
                 logits_lat_ms=logits_lat_ms,
                 token_ids=tokens,
                 mask=mask_for_sampling,
@@ -302,6 +327,7 @@ class Engine(ABC):
                 k=self._top_k,
                 force_return_unmasked_probs=echo,
             )
+            last_temperature = ll_response.temperature
 
             if can_finish_early and not mask[engine_output.issued_token.token_id]:
                 # Type checker needs some help
@@ -310,8 +336,8 @@ class Engine(ABC):
 
     def get_next_token_with_top_k(
         self,
-        logits: Optional[np.ndarray],
-        logits_lat_ms: Optional[float],
+        logits: NDArray,
+        logits_lat_ms: float,
         token_ids: list[int],
         mask: Optional[bytes],
         temperature: float,
@@ -322,7 +348,7 @@ class Engine(ABC):
 
         Parameters
         -------
-        logits : Optional[np.ndarray]
+        logits : Optional[NDArray]
             The logits for the current token ids in the sequence.
             If None, the model will call get_logits to get the logits.
         logits_lat_ms: Optional[float]
@@ -345,38 +371,7 @@ class Engine(ABC):
             The output from the model.
         """
 
-        if logits is None:
-            t0 = time.time()
-            try:
-                logits = self.get_logits(token_ids=token_ids)
-            except NotImplementedError:
-                # fallback to orignal get_next_token method
-                _t0 = time.time()
-                token_id = self.get_next_token(
-                    token_ids=token_ids,
-                    mask=mask,
-                    temperature=temperature,
-                )
-                _lat = (time.time() - _t0) * 1000
-
-                _issued_token = GenToken(
-                    token_id=token_id,
-                    prob=1.0,
-                    bytes=self.tokenizer.decode([token_id]),
-                    latency_ms=_lat,
-                    is_generated=True,
-                )
-
-                return EngineOutput(
-                    issued_token=_issued_token,
-                    top_k=[_issued_token],
-                    masked_top_k=[_issued_token] if mask is not None else None,
-                )
-            lat_ms = (time.time() - t0) * 1000
-        else:
-            lat_ms = logits_lat_ms
-
-        def get_top_k(_probs: np.ndarray, _k: int = 5) -> list[GenToken]:
+        def get_top_k(_probs: NDArray, _k: int = 5) -> list[GenToken]:
             top_k_indices = np.argsort(_probs)[::-1][:_k]
             top_k_probs = _probs[top_k_indices]
 
@@ -385,7 +380,7 @@ class Engine(ABC):
                     token_id=token,
                     prob=prob,
                     bytes=self.tokenizer.decode([token]),
-                    latency_ms=lat_ms,
+                    latency_ms=logits_lat_ms,
                     is_generated=True,
                 )
                 for token, prob in zip(top_k_indices, top_k_probs)
@@ -435,7 +430,7 @@ class Engine(ABC):
                 token_id=sampled_index,
                 prob=sampled_prob,
                 bytes=self.tokenizer.decode([sampled_index]),
-                latency_ms=lat_ms,
+                latency_ms=logits_lat_ms,
                 is_generated=True,
             )
 
@@ -449,15 +444,11 @@ class Engine(ABC):
         return output
 
     @abstractmethod
-    def get_logits(self, token_ids: list[int]) -> np.ndarray:
+    def get_logits(self, token_ids: list[int], full_sequence: bool = False) -> NDArray:
         pass
 
-    def get_per_token_topk_probs(self, token_ids: list[int], top_k: int = 5) -> list[GenToken]:
-        """Get the top-k probabilities for each token in the sequence."""
-        raise NotImplementedError
-
     def sample_with_temperature(
-        self, logits: np.ndarray, mask: Optional[bytes], temperature: float
+        self, logits: NDArray, mask: Optional[bytes], temperature: float
     ) -> int:
         if mask is not None:
             logits += np.frombuffer(mask, dtype=np.uint8)

commit d5d381f75f59c079c666288509c813dfa8f7d0d7

[Hotfix] Fix performance regression introduced in #1261 (#1276)
#1261 added a logit cache, which was being concatenated to in a loop.

This PR never caches any logits except for the final one, but if
`include_all_uncached_tokens` is `True`, then `get_logits` will return
an array of shape `(len(tokens)-num_cached, vocab_size)`. Otherwise, it
will return an array of shape `(1, vocab_size)`

This "should" include all tokens being fast-forwarded (unless the
fast-forwarded tokens are somehow already in the kv, e.g. if a notebook
cell is re-run). If any are missing, I just call the probabilities `nan`
here, which could in principle cause some artifacts in the widget.

Note that an alternative implementation would have been to keep the full
logit cache, pre-allocating the buffer in advance with shape `(n_ctx,
vocab_size)`, but this feels really unnecessary. Happy to revisit after
we get this merged and the big performance issues are gone.

@nopdive would you mind re-running the repro? Note that you shouldn't
see a speedup for the prompt part (as this PR does not concern
pre-filling), but you should see a speedup for the rest of the
completion. Side note, but we can get rid of the (relatively small, mind
you) prompt slowdown by simply deciding to omit probabilities from
"input" tokens. Lmk if you want that, and I can open a follow-up PR.

Edit:
Intended to fix #1275

guidance/models/_engine/_engine.py
@@ -212,9 +212,8 @@ class Engine(ABC):
                 logits_lat_ms = 0.0
             else:
                 t1 = time.time()
-                logits = self.get_logits(token_ids=tokens, full_sequence=True)
+                logits = self.get_logits(token_ids=tokens, include_all_uncached_tokens=True)
                 logits_lat_ms = (time.time() - t1) * 1000
-
 
             # Important: don't wait on this future until after getting the logits;
             # this allows the mask to be built concurrently with model inference
@@ -229,19 +228,27 @@ class Engine(ABC):
 
             if logits is not None:
                 # Not the last one -- that's for the *next* token.
-                ff_logits = logits[max(len(logits)-len(ff_tokens)-1, 0):-1]
+                ff_logits = logits[-len(ff_tokens)-1:-1]
                 ff_probs = (
-                    softmax(np.array(ff_logits))
+                    softmax(ff_logits)
                     if last_temperature < _TEMPERATURE_EPSILON
-                    else softmax(np.array(ff_logits) / last_temperature)
+                    else softmax(ff_logits / last_temperature)
                 )
-                if ff_probs.shape[0] != len(ff_tokens):
-                    assert ff_probs.shape[0] == len(ff_tokens) - 1
+
+                if len(ff_tokens) == len(tokens) and ff_probs.shape[0] == len(ff_tokens) - 1:
                     # We didn't have a BOS token, so we need to fake the first token prob (say... 1?)
-                    ff_probs = np.concatenate(
-                        (np.ones((1, ff_probs.shape[1])), ff_probs), axis=0
+                    ff_probs = np.pad(
+                        ff_probs, [(1, 0), (0, 0)], mode='constant', constant_values=1.0
+                    )
+                elif ff_probs.shape[0] < len(ff_tokens):
+                    # Not enough logits were returned despite include_all_uncached_tokens=True, probably due to
+                    # using a mock model that doesn't bother to return logits for uncached tokens (all are uncached...)
+                    ff_probs = np.pad(
+                        ff_probs, [(len(ff_tokens) - ff_probs.shape[0], 0), (0, 0)],
+                        mode='constant', constant_values=np.nan
                     )
             else:
+                # really just for mypy -- we shouldn't need this
                 ff_probs = np.empty(shape=())
 
             gen_tokens = []
@@ -448,5 +455,14 @@ class Engine(ABC):
         return output
 
     @abstractmethod
-    def get_logits(self, token_ids: list[int], full_sequence: bool = False) -> NDArray:
+    def get_logits(self, token_ids: list[int], include_all_uncached_tokens: bool = False) -> NDArray:
+        """
+        Get the logits for the given token ids.
+        If include_all_uncached_tokens is True:
+            logits for all uncached tokens will be returned, i.e.
+            the return value's shape will be `(len(tokens)-num_cached, vocab_size)`.
+        If include_all_uncached_tokens is False:
+            logits for the last token will be returned, i.e.
+            the return value's shape will be `(1, vocab_size)`.
+        """
         pass

guidance/models/_llama_cpp.py
@@ -148,7 +148,7 @@ class LlamaCppEngine(Engine):
                          compute_log_probs=compute_log_probs, enable_backtrack=enable_backtrack,
                          enable_ff_tokens=enable_ff_tokens, enable_monitoring=enable_monitoring, **kwargs)
 
-    def get_logits(self, token_ids, full_sequence=False):
+    def get_logits(self, token_ids: list[int], include_all_uncached_tokens: bool = False) -> np.ndarray:
         """Computes the logits for the given token state.
 
         This overrides a method from the LocalEngine class that is used to get
@@ -167,20 +167,17 @@ class LlamaCppEngine(Engine):
         # check what we have already cached
         num_cached = sum(takewhile(operator.truth, map(operator.eq, token_ids, self._cached_token_ids)))
         if num_cached == len(token_ids):
-            if full_sequence:
-                return self._cached_logits[:num_cached, :]
-            else:
-                return self._cached_logits[[num_cached - 1], :]
+            if num_cached == len(self._cached_token_ids):
+                # last token input is the same as the last cached token, so return the last cached logits
+                return self._cached_logits
+            # we need to pass at least one new token
+            num_cached = num_cached - 1
 
         # clear obsolete parts of kv cache
         llama_cpp.llama_kv_cache_seq_rm(self.model_obj.ctx, -1, num_cached, -1)
 
-        # clear obsolete parts of logit cache
-        if self._cached_logits is not None and len(self._cached_logits) > num_cached:
-            self._cached_logits = self._cached_logits[:num_cached, :]
-
         # eval the model
-        logits_for_each_batch = []
+        logits_for_each_batch: list[np.ndarray] = []
         n_batch = self.model_obj.n_batch
         batch = self._context.batch
         for i in range(num_cached, len(token_ids), n_batch):
@@ -199,11 +196,11 @@ class LlamaCppEngine(Engine):
 
             # get the logits for *this* batch
             llama_logits = llama_cpp.llama_get_logits(self.model_obj.ctx)
-            logits = np.ctypeslib.as_array(
+            logits_for_this_batch = np.ctypeslib.as_array(
                 llama_logits,
                 shape=(n_tokens, self._n_vocab),
             ).copy()
-            logits_for_each_batch.append(logits)
+            logits_for_each_batch.append(logits_for_this_batch)
 
         # update the metrics
         self.metrics.engine_output_tokens += 1
@@ -211,19 +208,18 @@ class LlamaCppEngine(Engine):
 
         # save the results
         self._cached_token_ids = token_ids.copy()
+        self._cached_logits = logits_for_each_batch[-1][[-1], :]
 
-        if self._cached_logits is not None:
-            logits_for_each_batch = [self._cached_logits] + logits_for_each_batch
-
-        self._cached_logits = np.concatenate(
-            logits_for_each_batch,
-            axis=0
-        )
-
-        if full_sequence:
-            return self._cached_logits
+        if include_all_uncached_tokens:
+            logits = np.concatenate(
+                logits_for_each_batch,
+                axis=0
+            )
+            assert logits.shape[0] == len(token_ids) - num_cached
+            return logits
         else:
-            return self._cached_logits[[-1], :]
+            return self._cached_logits
+
 
 class LlamaCpp(Model):
     def __init__(

guidance/models/_mock.py
@@ -112,21 +112,7 @@ class MockEngine(Engine):
             logits, logits_lat_ms, token_ids, mask, temperature, k, force_return_unmasked_probs
         )
 
-    def get_logits(self, token_ids: list[int], full_sequence: bool = False) -> np.ndarray:
-        """Get the logits for the given token state."""
-        if not full_sequence:
-            return self._get_logits(token_ids).reshape(1, -1)
-        else:
-            # TODO: is it worth it to add a prefix cache here?
-            l0 = self._get_logits([token_ids[0]])
-            # if we are getting the full sequence then we need to compute the logits for all tokens
-            logits = np.zeros((len(token_ids), len(l0)))
-            logits[0] = l0
-            for i in range(1, len(token_ids)):
-                logits[i] = self._get_logits(token_ids[: i + 1])
-            return logits
-
-    def _get_logits(self, token_ids: list[int]) -> np.ndarray:
+    def get_logits(self, token_ids: list[int], include_all_uncached_tokens: bool = False) -> np.ndarray:
         """Pretends to compute the logits for the given token state."""
         if len(token_ids) == 0:
             raise ValueError("token_ids must not be empty")
@@ -154,7 +140,7 @@ class MockEngine(Engine):
                         logits[i] += bias
                         bias /= 2  # if we have multiple matches then they apply with decreasing bias
 
-        return logits
+        return logits.reshape(1, -1)
 
     def _get_next_tokens(self, byte_string):
         special_tokens = [

guidance/models/_transformers.py
@@ -459,7 +459,7 @@ class TransformersEngine(Engine):
             model = transformers_package.AutoModelForCausalLM.from_pretrained(model, **kwargs)
         return model
 
-    def get_logits(self, token_ids: list[int], full_sequence: bool = False):
+    def get_logits(self, token_ids: list[int], include_all_uncached_tokens: bool = False) -> np.ndarray:
         """Computes the logits for the given token state.
 
         This overrides a method from the LocalEngine class that is used to get
@@ -478,10 +478,11 @@ class TransformersEngine(Engine):
         # check what we have already cached
         num_cached = sum(takewhile(operator.truth, map(operator.eq, token_ids, self._cached_token_ids)))
         if num_cached == len(token_ids):
-            if full_sequence:
-                return self._cached_logits[:num_cached, :]
-            else:
-                return self._cached_logits[[num_cached - 1], :]
+            if num_cached == len(self._cached_token_ids):
+                # last token input is the same as the last cached token, so return the last cached logits
+                return self._cached_logits
+            # we need to pass at least one new token
+            num_cached = num_cached - 1
 
         # check how many tokens are in the kv cache and what the max size of the cache is
         past_key_values = self._past_key_values
@@ -558,17 +559,16 @@ class TransformersEngine(Engine):
                     else:
                         self._past_key_values = None
                     past_length = 0
-
-        # clear obsolete parts of logit cache
-        if self._cached_logits is not None and len(self._cached_logits) > num_cached:
-            self._cached_logits = self._cached_logits[:num_cached, :]
+        else:
+            # Should never happen, but just in case -- we're not using all of the cached tokens
+            # (because they're not really in the kv cache somehow)
+            num_cached = past_length
 
         # eval the model
-        assert past_length <= num_cached
-        assert num_cached <= len(token_ids)
+        assert past_length <= len(token_ids)
         new_token_ids = token_ids[past_length:]
         assert len(new_token_ids) > 0
-        logits_for_each_batch = []
+        logits_for_each_batch: list[np.ndarray] = []
         with torch.no_grad():
             # Not all models support batched tokens for some reason
             try:
@@ -628,19 +628,17 @@ class TransformersEngine(Engine):
         # save the results
         self._past_key_values = model_out.past_key_values
         self._cached_token_ids = token_ids.copy()
+        self._cached_logits = logits_for_each_batch[-1][[-1], :]
 
-        if self._cached_logits is not None:
-            logits_for_each_batch = [self._cached_logits] + logits_for_each_batch
-
-        self._cached_logits = np.concatenate(
-            logits_for_each_batch,
-            axis=0
-        )
-
-        if full_sequence:
-            return self._cached_logits
+        if include_all_uncached_tokens:
+            logits = np.concatenate(
+                logits_for_each_batch,
+                axis=0
+            )
+            assert logits.shape[0] == len(token_ids) - num_cached
+            return logits
         else:
-            return self._cached_logits[[-1], :]
+            return self._cached_logits
 
 class Transformers(Model):
     def __init__(