f43a0dc4c99f257a604e706211620f1e608326d9
Loc Huynh <jc1da.3011@gmail.com>
2025-06-24 15:30:36 -0700

    remove commented code

--- guidance/models/_engine/_engine.py
+++ guidance/models/_engine/_engine.py
@@ -405,47 +405,6 @@ class Engine(ABC):
             return sorted(
                 top_k_tokens, key=lambda x: x.prob, reverse=True
             )
-            
-        # def apply_top_k_only(_logits: NDArray, _k: int) -> NDArray:
-        #     indices_to_remove = _logits.argpartition(-_k)[:-_k]
-        #     _logits[indices_to_remove] = -float("inf")
-        #     return _logits
-            
-
-        # def apply_top_k_and_top_p_filter(_logits: NDArray, _sampling_params: Optional[SamplingParams]) -> NDArray:
-        #     if _sampling_params is None:
-        #         return _logits
-            
-        #     _top_p = _sampling_params.get("top_p", None)
-        #     _top_k = _sampling_params.get("top_k", None)
-
-        #     if _top_k is None and _top_p is None:
-        #         # No filtering, return logits as is
-        #         return _logits
-
-        #     if _top_k is not None and _top_p is None:
-        #         return apply_top_k_only(_logits, _top_k)
-
-        #     # try our best to sort logits one time only
-        #     sorted_logits = _logits.argsort()
-        #     if _top_k is not None:
-        #         indices_to_remove = sorted_logits[:-_top_k]
-        #         _logits[indices_to_remove] = -float("inf")
-
-        #     if _top_p is not None:
-        #         sorted_indices = sorted_logits[::-1]
-        #         sorted_logits = _logits[sorted_indices]
-        #         probs = softmax(sorted_logits)
-        #         cumulative_probs = np.cumsum(probs)
-        #         indices_to_remove = cumulative_probs > _top_p
-        #         if np.any(indices_to_remove):
-        #             # -1 to keep the first token that exceeds the threshold
-        #             first_to_remove = np.argmax(indices_to_remove) - 1
-        #             # make sure we always keep at least one token
-        #             sorted_indices_to_remove = sorted_indices[max(1, first_to_remove):]
-        #             _logits[sorted_indices_to_remove] = -float("inf")
-                
-        #     return _logits
 
         # compute top-k without masking
         probs = (
