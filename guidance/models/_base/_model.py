229b023d3eb93c580c32ff0200e0e6ac57b248e8
Loc Huynh <jc1da.3011@gmail.com>
2025-06-27 10:05:32 -0700

    Remove unnecessary merged code

--- guidance/models/_base/_model.py
+++ guidance/models/_base/_model.py
@@ -71,7 +71,6 @@ class Model:
 
         self._interpreter = interpreter
         self._active_blocks: dict[Block, int] = {}
-        self.token_count: int = 0
         self.sampling_params: Optional[SamplingParams] = sampling_params
 
         self._parent: Optional["Model"] = None
@@ -135,9 +134,6 @@ class Model:
             self._update_trace_node(self._id, self._parent_id, StatelessGuidanceInput(value=node))
 
         for i, output_attr in enumerate(self._interpreter.run(node, sampling_params=self.sampling_params)):
-            if isinstance(output_attr, TokenOutput) and not output_attr.is_input:
-                # TODO: put this elsewhere (inside state?)
-                self.token_count += 1
             if i != 0:
                 # On the first iteration, we already have a fresh trace node
                 # TODO: should be allowed to associate multiple output_attrs with a single input node?
