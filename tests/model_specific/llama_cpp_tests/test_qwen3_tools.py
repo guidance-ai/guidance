516e86f16c7ebc749557619d5cbc5ffbfbe0a32c
Hudson Cooper <hncooper96@gmail.com>
2025-06-30 13:20:55 -0700

    check with jinja

--- tests/model_specific/llama_cpp_tests/test_qwen3_tools.py
+++ tests/model_specific/llama_cpp_tests/test_qwen3_tools.py
@@ -1,9 +1,12 @@
+from json import dumps
 from typing import Optional
 
 import pytest
+from jinja2.environment import Environment
 
 import guidance
 from guidance import assistant, gen, system, user
+from guidance._ast import ToolDefinition
 from guidance.tools import Qwen3ToolCallHandler
 
 
@@ -18,11 +21,10 @@ def qwen3(selected_model, selected_model_name):
 def test_qwen3_tool(qwen3: guidance.models.Model):
     lm = qwen3
     lm._interpreter.tool_call_handler_cls = Qwen3ToolCallHandler
-    called = False
+    called_args = []
 
     def trending_songs(n: int, genre: Optional[str] = None) -> list[str]:
-        nonlocal called
-        called = True
+        called_args.append({"n": n, "genre": genre})
         return ["Song 1", "Song 2", "Song 3"]
 
     with system():
@@ -42,9 +44,38 @@ For each function call, return a json object with function name and arguments wi
 </tool_call>"""
 
     with user():
-        lm += """\
-Use tools to get latest trending songs."""
+        lm += """What are the latest trending songs?"""
 
     with assistant():
         lm += gen(tools=[trending_songs])
-    assert called
+
+    with assistant():
+        lm += gen(name="follow_up")
+
+    assert called_args
+
+    jinja_env = Environment().from_string(lm._interpreter.engine.model_obj.metadata["tokenizer.chat_template"])
+    tc = {"name": "trending_songs", "arguments": called_args[0]}
+    jinja2_render = jinja_env.render(
+        messages=[
+            {"role": "user", "content": "What are the latest trending songs?"},
+            {"role": "assistant", "content": f"<tool_call>\n{dumps(tc)}\n</tool_call>"},
+            {"role": "tool", "content": dumps(["Song 1", "Song 2", "Song 3"])},
+            {"role": "assistant", "content": lm["follow_up"]},
+        ],
+        tools=[ToolDefinition.from_callable(trending_songs).args.model_json_schema()],
+    )
+
+    # Compare the tokenization of the strings, rather than the strings
+    # themselves (e.g. `<|user|>` may tokenize the same as `<|user|>\n`)
+    lm_tokens = lm._interpreter.engine.tokenizer.encode(str(lm).encode())
+    jinja2_tokens = lm._interpreter.engine.tokenizer.encode(jinja2_render.encode())
+
+    # Only check substring due to BOS/EOS tokens, unfinished closing tags
+    diff = len(jinja2_tokens) - len(lm_tokens)
+    assert diff >= 0
+    for i in range(diff + 1):
+        if jinja2_tokens[i : i + len(lm_tokens)] == lm_tokens:
+            break
+    else:
+        raise AssertionError("lm mismatches jinja template", str(lm), str(jinja2_render))
