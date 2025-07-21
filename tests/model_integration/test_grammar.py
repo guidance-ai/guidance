3df9576fca947d35d59032430f4548f9a7ab62f7
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2025-07-21 14:31:49 -0400

    Add test from issue

--- tests/model_integration/test_grammar.py
+++ tests/model_integration/test_grammar.py
@@ -6,3 +6,14 @@ def test_select_simple(selected_model: models.Model):
     options = ["baad I think", "bad I think", "bad"]
     lm = lm + "Scott is quite " + select(name="bad", options=options)
     assert lm["bad"] in options
+
+
+def test_select_large(selected_model: models.Model):
+    lm = selected_model
+    n = 2002
+
+    lm += "Pick a number"
+    lm += select(list(range(n)), name="response")
+
+    result = int(lm["response"])
+    assert 0 <= result < n
