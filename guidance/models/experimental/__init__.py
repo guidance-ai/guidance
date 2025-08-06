c6243a0b7ea316d5411ceea00cdf44d224e07cb9
JC1DA <jc1da.3011@gmail.com>
2025-08-06 10:32:51 -0700

    Add experimental sglang support (#1340)

--- guidance/models/experimental/__init__.py
+++ guidance/models/experimental/__init__.py
@@ -1,7 +1,5 @@
 from ._litellm import LiteLLM
+from ._sglang import SglangModel
 from ._vllm import VLLMModel
 
-__all__ = [
-    "LiteLLM",
-    "VLLMModel",
-]
+__all__ = ["LiteLLM", "SglangModel", "VLLMModel"]
