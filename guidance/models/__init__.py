28ce32003b2d90a1db71182f55bf26603fb7f2ce
Loc Huynh <jc1da.3011@gmail.com>
2025-09-15 14:11:22 -0700

    format code

--- guidance/models/__init__.py
+++ guidance/models/__init__.py
@@ -3,9 +3,9 @@ from ._azureai import create_azure_aifoundry_model, create_azure_openai_model
 from ._base import Model
 from ._llama_cpp import LlamaCpp
 from ._mock import Mock
+from ._onnxruntime import OnnxRuntimeGenAI
 from ._openai import OpenAI
 from ._transformers import Transformers
-from ._onnxruntime import OnnxRuntimeGenAI
 
 __all__ = [
     "LlamaCpp",
