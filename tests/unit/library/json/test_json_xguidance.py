04913b2a1530d56eaa1e3060d31ee2b88f7998e8
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2025-11-06 15:30:23 -0500

    Better test ideas

--- tests/unit/library/json/test_json_xguidance.py
+++ tests/unit/library/json/test_json_xguidance.py
@@ -10,7 +10,7 @@ from .utils import check_match_failure
 
 class TestXGuidance:
     @pytest.mark.parametrize("desired_indent", [None, 2, 4])
-    @pytest.mark.parametrize("separators", [None, (",", ":"), (", ", ": "), (",     ", ":      ")])
+    @pytest.mark.parametrize("separators", [None, (",", ":"), (", ", ": "), (" ,  ", " :  ")])
     def test_whitespace_flexible(self, desired_indent, separators):
         schema = """{
             "type": "object",
